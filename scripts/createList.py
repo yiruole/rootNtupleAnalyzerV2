e18e6ddd08dc5e6b615e3ca3022222c4b744012d
ferencek <>
2010-07-07 11:12:44 +0000

    added ability to handle multiple input directories

--- scripts/createList.py
+++ scripts/createList.py
@@ -2,79 +2,71 @@
 
 import os, sys, optparse, subprocess, string, re
 
-# for files on Castor
-def filelist_castor(inputDir, match):
+# needed for having multiple arguments per option flag
+def cb(option, opt_str, value, parser):
+    args=[]
+    for arg in parser.rargs:
+        if arg[0] != "-":
+            args.append(arg)
+        else:
+            del parser.rargs[:len(args)]
+            break
+    if len(args)==0:
+        args=None
+    if getattr(parser.values, option.dest):
+        args.extend(getattr(parser.values, option.dest))
+    setattr(parser.values, option.dest, args)
+
+
+def unique(keys):
+    unique = []
+    for i in keys:
+        if i not in unique:
+            unique.append(i)
+    return unique
+
+
+def make_filenamelist_castor(inputDir):
+    filenamelist = []
     proc = subprocess.Popen( [ 'rfdir', inputDir ], stdout = subprocess.PIPE, stderr = subprocess.STDOUT )
     output = proc.communicate()[0]
     if proc.returncode != 0:
         print output
         sys.exit(1)
-
-    filelist = {}
-
     for line in output.splitlines():
-        filename = line.strip().split()[8]
-        if( not re.search('.root$', filename) ):
-            continue
-        if ( match!=None and not re.search(match, filename) ):
-            continue
-        m1 = re.search('_\d+_\d+_\w+.root', filename)
-        m2 = re.search('_\d+_\d+.root', filename)
-        if( m1 ):
-            dataset = re.split('_\d+_\d+_\w+.root', filename)[0]
-            job = filename[m1.start():].lstrip('_').replace('.root','').split('_')
-            if dataset not in filelist.keys():
-                filelist[dataset] = {}
-                if int(job[0]) not in filelist[dataset].keys():
-                    filelist[dataset][int(job[0])] = [[int(job[1])],[job[2]]]
-                else:
-                    filelist[dataset][int(job[0])][0].append(int(job[1]))
-                    filelist[dataset][int(job[0])][1].append(job[2])
-            else:
-                if int(job[0]) not in filelist[dataset].keys():
-                    filelist[dataset][int(job[0])] = [[int(job[1])],[job[2]]]
-                else:
-                    filelist[dataset][int(job[0])][0].append(int(job[1]))
-                    filelist[dataset][int(job[0])][1].append(job[2])
-        elif( m2 ):
-            dataset = re.split('_\d+_\d+.root', filename)[0]
-            job = filename[m2.start():].lstrip('_').replace('.root','').split('_')
-            if dataset not in filelist.keys():
-                filelist[dataset] = {}
-                if int(job[0]) not in filelist[dataset].keys():
-                    filelist[dataset][int(job[0])] = [[int(job[1])],[]]
-                else:
-                    filelist[dataset][int(job[0])][0].append(int(job[1]))
-            else:
-                if int(job[0]) not in filelist[dataset].keys():
-                    filelist[dataset][int(job[0])] = [[int(job[1])],[]]
-                else:
-                    filelist[dataset][int(job[0])][0].append(int(job[1]))
-        else:
-            dataset = re.split('_\d+.root', filename)[0]
-            job = filename[re.search('_\d+.root', filename).start():].lstrip('_').replace('.root','').split('_')
-            if dataset not in filelist.keys():
-                filelist[dataset] = {}
-                if int(job[0]) not in filelist[dataset].keys():
-                    filelist[dataset][int(job[0])] = [[],[]]
-            else:
-                if int(job[0]) not in filelist[dataset].keys():
-                    filelist[dataset][int(job[0])] = [[],[]]
+        filenamelist.append(line.strip().split()[8])
 
-    return filelist
+    return filenamelist
 
 
-# for files on locally mounted disk space
-def filelist_default(inputDir, match):
+def make_filenamelist_default(inputDir):
     if not os.path.isdir(inputDir):
         print ('%s is not a directory'%(inputDir))
         sys.exit(1)
 
-    filelist = {}
-
+    filenamelist = []
     for filename in os.listdir(inputDir):
         if not os.path.isfile(os.path.join(inputDir,filename)):
             continue
+        filenamelist.append(filename)
+
+    return filenamelist
+
+
+def process_input_dir(inputDir, match, filelist):
+    inputDir = inputDir.rstrip('/')+'/'
+    prefix = ''
+    filenamelist = []
+
+    if( re.search("^/castor/cern.ch/", inputDir) ):
+        prefix = 'rfio:'
+        filenamelist = make_filenamelist_castor(inputDir)
+    else:
+        filenamelist = make_filenamelist_default(inputDir)
+
+    path = prefix+inputDir;
+
+    for filename in filenamelist:
         if( not re.search('.root$', filename) ):
             continue
         if ( match!=None and not re.search(match, filename) ):
@@ -86,47 +78,53 @@ def filelist_default(inputDir, match):
             job = filename[m1.start():].lstrip('_').replace('.root','').split('_')
             if dataset not in filelist.keys():
                 filelist[dataset] = {}
-                if int(job[0]) not in filelist[dataset].keys():
-                    filelist[dataset][int(job[0])] = [[int(job[1])],[job[2]]]
-                else:
-                    filelist[dataset][int(job[0])][0].append(int(job[1]))
-                    filelist[dataset][int(job[0])][1].append(job[2])
+                filelist[dataset][path] = {}
+                filelist[dataset][path][int(job[0])] = [[int(job[1])],[job[2]]]
             else:
-                if int(job[0]) not in filelist[dataset].keys():
-                    filelist[dataset][int(job[0])] = [[int(job[1])],[job[2]]]
+                if path not in filelist[dataset].keys():
+                    filelist[dataset][path] = {}
+                    filelist[dataset][path][int(job[0])] = [[int(job[1])],[job[2]]]
                 else:
-                    filelist[dataset][int(job[0])][0].append(int(job[1]))
-                    filelist[dataset][int(job[0])][1].append(job[2])
+                    if int(job[0]) not in filelist[dataset][path].keys():
+                        filelist[dataset][path][int(job[0])] = [[int(job[1])],[job[2]]]
+                    else:
+                        filelist[dataset][path][int(job[0])][0].append(int(job[1]))
+                        filelist[dataset][path][int(job[0])][1].append(job[2])
         elif( m2 ):
             dataset = re.split('_\d+_\d+.root', filename)[0]
             job = filename[m2.start():].lstrip('_').replace('.root','').split('_')
             if dataset not in filelist.keys():
                 filelist[dataset] = {}
-                if int(job[0]) not in filelist[dataset].keys():
-                    filelist[dataset][int(job[0])] = [[int(job[1])],[]]
-                else:
-                    filelist[dataset][int(job[0])][0].append(int(job[1]))
+                filelist[dataset][path] = {}
+                filelist[dataset][path][int(job[0])] = [[int(job[1])],[]]
             else:
-                if int(job[0]) not in filelist[dataset].keys():
-                    filelist[dataset][int(job[0])] = [[int(job[1])],[]]
+                if path not in filelist[dataset].keys():
+                    filelist[dataset][path] = {}
+                    filelist[dataset][path][int(job[0])] = [[int(job[1])],[]]
                 else:
-                    filelist[dataset][int(job[0])][0].append(int(job[1]))
+                    if int(job[0]) not in filelist[dataset][path].keys():
+                        filelist[dataset][path][int(job[0])] = [[int(job[1])],[]]
+                    else:
+                        filelist[dataset][path][int(job[0])][0].append(int(job[1]))
         else:
             dataset = re.split('_\d+.root', filename)[0]
             job = filename[re.search('_\d+.root', filename).start():].lstrip('_').replace('.root','').split('_')
             if dataset not in filelist.keys():
                 filelist[dataset] = {}
-                if int(job[0]) not in filelist[dataset].keys():
-                    filelist[dataset][int(job[0])] = [[],[]]
+                filelist[dataset][path] = {}
+                filelist[dataset][path][int(job[0])] = [[],[]]
             else:
-                if int(job[0]) not in filelist[dataset].keys():
-                    filelist[dataset][int(job[0])] = [[],[]]
+                if path not in filelist[dataset].keys():
+                    filelist[dataset][path] = {}
+                    filelist[dataset][path][int(job[0])] = [[],[]]
+                else:
+                    if int(job[0]) not in filelist[dataset][path].keys():
+                        filelist[dataset][path][int(job[0])] = [[],[]]
 
-    return filelist
+    return
 
 
-def write_inputlists(filelist, prefix, inputDir, outputDir):
-    inputDir = inputDir.rstrip('/')+'/'
+def write_inputlists(filelist, outputDir):
     outputDir = outputDir.rstrip('/')+'/'
 
     keys = filelist.keys()
@@ -142,42 +140,49 @@ def write_inputlists(filelist, prefix, inputDir, outputDir):
         inputListName = outputDir+dataset+'.txt'
         mainInputList.write(inputListName+'\n')
         inputList = open(inputListName,'w')
-        for job in filelist[dataset].keys():
-            if( len(filelist[dataset][job][0])>0 ):
-                if( len(filelist[dataset][job][1])>0 ):
-                    filename = (prefix+inputDir+dataset+'_%i_%i_%s.root')%(job,max(filelist[dataset][job][0]),filelist[dataset][job][1][filelist[dataset][job][0].index(max(filelist[dataset][job][0]))])
+        for path in filelist[dataset].keys():
+            for job in filelist[dataset][path].keys():
+                if( len(filelist[dataset][path][job][0])>0 ):
+                    if( len(filelist[dataset][path][job][1])>0 ):
+                        filename = (path+dataset+'_%i_%i_%s.root')%(job,max(filelist[dataset][path][job][0]),filelist[dataset][path][job][1][filelist[dataset][path][job][0].index(max(filelist[dataset][path][job][0]))])
+                    else:
+                        filename = (path+dataset+'_%i_%i.root')%(job,max(filelist[dataset][path][job][0]))
                 else:
-                    filename = (prefix+inputDir+dataset+'_%i_%i.root')%(job,max(filelist[dataset][job][0]))
-            else:
-                filename = (prefix+inputDir+dataset+'_%i.root')%(job)
-            inputList.write(filename+'\n')
+                    filename = (path+dataset+'_%i.root')%(job)
+                inputList.write(filename+'\n')
+        inputList.close()
+    mainInputList.close()
+
+    return
 
 
 def main():
     parser = optparse.OptionParser(
-        usage='Usage: %prog [-m MATCH] -i INPUTDIR -o OUTPUTDIR',
-        description='Example: createList.py -i /castor/cern.ch/user/f/ferencek/LQ/RootNtuple/RootNtuple-V00-00-08-MC-LQ-eejj_20100518_231412 -o /home/santanas/Workspace/Leptoquarks/rootNtupleAnalyzer/config')
+        usage='Usage: %prog [-m MATCH] -i INPUTDIR(S) -o OUTPUTDIR',
+        description='Example: createList.py -i /castor/cern.ch/user/f/ferencek/LQ/RootNtuple/RootNtuple-V00-00-08-MC-LQ-eejj_20100518_231412 -o /home/santanas/Workspace/Leptoquarks/rootNtupleAnalyzer/config'
+    )
     parser.add_option( '-m', '--match', metavar='MATCH', action='store', help='Only files containing the MATCH string in their names will be considered' )
-    parser.add_option( '-i', '--inputDir', metavar='INPUTDIR', action='store', help='Specifies the input directory containing .root files. Please use the full path. Castor directories are also supported' )
+    parser.add_option( '-i', '--inputDirs', metavar='INPUTDIR(S)', action="callback", callback=cb, dest="inputDirs", help='Specifies the input directory (or directories separated by space) containing .root files. Please use the full path. Castor directories are also supported' )
     parser.add_option( '-o', '--outputDir', metavar='OUTPUTDIR', action='store', help='Specifies the output directory where the .txt list files will be stored. Please use the full path' )
 
     (options, args) = parser.parse_args(args=None)
 
-    if ( options.inputDir==None or options.outputDir==None ):
-        print ("Options -i and -o are required\n")
+    if ( options.inputDirs==None or options.outputDir==None ):
+        print ("\nOptions -i and -o are required\n")
         parser.print_help()
         sys.exit()
 
     filelist = {}
 
-    if( re.search("^/castor/cern.ch/", options.inputDir) ):
-        filelist = filelist_castor(options.inputDir, options.match)
-        write_inputlists(filelist, 'rfio:', options.inputDir, options.outputDir)
-    else:
-        filelist = filelist_default(options.inputDir, options.match)
-        write_inputlists(filelist, '', options.inputDir, options.outputDir)
+    inputDirs = unique(options.inputDirs)
+
+    for inputDir in inputDirs:
+        process_input_dir(inputDir, options.match, filelist)
+
+    write_inputlists(filelist, options.outputDir)
 
     print 'Output files successfully created'
+    sys.exit()
 
 
 if __name__ == '__main__':
