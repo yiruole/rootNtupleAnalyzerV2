30dda167347f25efc34a979595221c9eb5ae698e
santanas <>
2010-09-05 09:21:26 +0000

    add variable for input list and sample list for merging

--- scripts/writeCommandsToRunOnMoreCutFiles.sh
+++ scripts/writeCommandsToRunOnMoreCutFiles.sh
@@ -14,15 +14,17 @@
 files=`ls $LQMACRO/config/eejj/cutTable_eejjSample_*.txt $LQMACRO/config/cutTable_eejjSample.txt` # list of cut files that will be used
 #files=`ls ../rootNtupleMacrosV2/config/cutTable_eejjSample.txt` # list of cut files that will be used
 OUTDIRPATH=$LQDATA  # a subdir will be created for each cut file 
-SUBDIR=eejj_analysis/1.1pb-1 # output sub-directory (i.e. output will be in OUTDIRPATH/SUBDIR)
+SUBDIR=eejj_analysis/1.1pb-1_v5_metStudy # output sub-directory (i.e. output will be in OUTDIRPATH/SUBDIR)
                              # it is suggested to specify the luminosity in the name of the directory
 ILUM=1.1 # integrated luminosity in pb-1 to be used for rescaling/merging MC samples
 FACTOR=1000 # numbers in final tables (but *not* in plots) will be multiplied by this scale factor (to see well the decimal digits)
 CODENAME=analysisClass_eejjSample #the actual name of the code used to process the ntuples (without the suffix ".C") 
-XSECTION=config/xsection_7TeV.txt #specify cross section file
-#XSECTION=config/xsection_7TeV_Zrescale.txt #specify cross section file
-NJOBS=30 #number of jobs for each dataset
-WAIT=5 #seconds of delay between submission of different datasets
+INPUTLIST=config/inputListAllCurrent.txt #specify input list
+#XSECTION=config/xsection_7TeV.txt #specify cross section file
+XSECTION=config/xsection_7TeV_Zrescale.txt #specify cross section file
+SAMPLELISTFORMERGING=config/sampleListForMerging_7TeV.txt #specify list for sample merging
+NJOBS=50 #number of jobs for each dataset
+WAIT=8 #seconds of delay between submission of different datasets
 
 #### END OF INPUTS ####
 
@@ -39,7 +41,7 @@ cat >> $COMMANDFILE <<EOF
 #### launch, check and combine cmds for $suffix ####
 
   ./scripts/launchAnalysis_batch.pl \
-    -i config/inputListAllCurrent.txt \
+    -i $INPUTLIST \
     -n rootTupleTree/tree \
     -c $file \
     -o $OUTDIRPATH/$SUBDIR/output_$suffix  \
@@ -49,7 +51,7 @@ cat >> $COMMANDFILE <<EOF
     | tee $OUTDIRPATH/$SUBDIR/output_$suffix/launch_${suffix}.log
 
   ./scripts/check_combine_output_batch.py \
-    -i config/inputListAllCurrent.txt \
+    -i $INPUTLIST \
     -c $CODENAME \
     -d $OUTDIRPATH/$SUBDIR/output_$suffix \
     -o $OUTDIRPATH/$SUBDIR/output_$suffix \
@@ -57,23 +59,23 @@ cat >> $COMMANDFILE <<EOF
     | tee $OUTDIRPATH/$SUBDIR/output_$suffix/checkcombine_${suffix}.log
 
   ./scripts/combineTablesTemplate.py \
-    -i config/inputListAllCurrent.txt \
+    -i $INPUTLIST \
     -c $CODENAME \
     -d $OUTDIRPATH/$SUBDIR/output_$suffix \
     -l  `echo "$ILUM*$FACTOR" | bc` \
     -x $XSECTION  \
     -o $OUTDIRPATH/$SUBDIR/output_$suffix \
-    -s config/sampleListForMerging_7TeV.txt \
+    -s $SAMPLELISTFORMERGING \
     | tee $OUTDIRPATH/$SUBDIR/output_$suffix/combineTables_${suffix}.log
 
   ./scripts/combinePlotsTemplate.py \
-    -i config/inputListAllCurrent.txt \
+    -i $INPUTLIST \
     -c $CODENAME \
     -d $OUTDIRPATH/$SUBDIR/output_$suffix \
     -l ${ILUM} \
     -x $XSECTION  \
     -o $OUTDIRPATH/$SUBDIR/output_$suffix \
-    -s config/sampleListForMerging_7TeV.txt \
+    -s $SAMPLELISTFORMERGING \
     | tee $OUTDIRPATH/$SUBDIR/output_$suffix/combinePlots_${suffix}.log
 
 EOF
