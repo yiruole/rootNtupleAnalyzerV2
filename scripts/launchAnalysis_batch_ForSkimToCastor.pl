55122b0746a3c2778907acf5580a3776c586fd13
santanas <>
2011-06-05 16:32:04 +0000

    new scripts to make skim from ntuples, and to store them to castor directly

---
+++ scripts/launchAnalysis_batch_ForSkimToCastor.pl
@@ -0,0 +1,182 @@
+#!/usr/bin/perl
+
+###########################################
+## Code to launch the analysis in batch  ##
+## and copy the skim output to castor    ##
+###########################################
+
+#--------------------------------------------------------------
+# Francesco Santanastasio  <francesco.santanastasio@cern.ch>
+#--------------------------------------------------------------
+
+print "Starting...\n";
+
+use Time::Local;
+use Getopt::Std;
+
+## input info
+
+my $inputList;
+my $outputDir;
+my $treename;
+my $cutfile;
+my $castorDir;
+
+getopts('h:i:o:n:c:j:q:w:d:');
+
+if(!$opt_i) {help();}
+if(!$opt_o) {help();}
+if(!$opt_n) {help();}
+if(!$opt_c) {help();}
+if(!$opt_j) {help();}
+if(!$opt_q) {help();}
+if(!$opt_d) {help();}
+#if(!$opt_w) {help();}
+
+
+if($opt_h) {help();}
+if($opt_i) {$inputList = $opt_i;}
+if($opt_o) {$outputDir = $opt_o;}
+if($opt_n) {$treename = $opt_n;}
+if($opt_c) {$cutfile = $opt_c;}
+if($opt_j) {$njobs = $opt_j;}
+if($opt_q) {$queue = $opt_q;}
+if($opt_w) {$wait = $opt_w;} else {$wait=0;}
+if($opt_d) {$castorDir = $opt_d;}
+
+system "mkdir -p $outputDir";
+system "cp $cutfile $outputDir/";
+
+#create CASTOR dir
+my $currentPath = $ENV{'CASTOR_HOME'};
+my $fullCastorPath = $currentPath."/".$castorDir;
+system("rfmkdir -p $fullCastorPath");
+
+##setting permission on CASTOR dir
+#print("rfchmod 755 $currentPath \n");
+#system("rfchmod 755 $currentPath");
+#my @directories = split(/\//, $castorDir);
+#foreach $dir(@directories)
+#{
+#    $currentPath = $currentPath."/".$dir;
+#    print("rfchmod 755 $currentPath \n");
+#    system("rfchmod 755 $currentPath");
+#}
+#print("rfchmod 775 $fullCastorPath \n");
+#system("rfchmod 775 $fullCastorPath");
+
+open CUTFILE, $cutfile or die $!;
+
+my $found_json = 0;
+my $jsonFile = "";
+
+while (<CUTFILE>) {
+    my $line = trim ($_);
+    my $first_four_characters = substr($line,0,4);
+    if (lc($first_four_characters) eq "json" )
+    {	
+	
+	@split_line = split (" ", $line) ;
+	$number_of_elements = $#split_line + 1;
+
+	if ( $found_json ) 
+	{
+	    print "ERROR: Please specify only one JSON file in your cut file!\n";
+	    exit 0;
+ 	}
+
+	if ( $#split_line != 1 ) 
+	{
+	    print "ERROR: Too many ($number_of_elements) elements in the line.  Line reads:\n";
+	    print "  $line\n";
+	    print "  Format in cut file should be:\n";
+	    print "  JSON <json file full path>\n";
+	    exit 0;
+	}
+
+	$jsonFile = @split_line[1];
+
+	open (JSONFILE, "<$jsonFile") || die ("ERROR: JSON file $jsonFile specified in $cutfile does not exist");
+	close (JSONFILE );
+   
+	$found_json=1
+    }    
+} 
+
+close (CUTFILE);
+
+if ( $found_json ) {
+    print  "cp $jsonFile $outputDir/\n";
+    system "cp $jsonFile $outputDir/";
+}
+
+open (INPUTLIST, "<$inputList") || die ("...error reading file $inputList $!");
+@inputList = <INPUTLIST>;
+#print @inputList;
+close(INPUTLIST);
+
+open (FILE, "ls -l src/analysisClass.C |") || die ("...error reading the file src/analysisClass.C $!");
+$analysisClassFull = <FILE>;
+close(FILE);
+
+## 1st split
+my @array = split(/\s+/ , $analysisClassFull );
+#print "$analysisClassFull\n";
+#print "$array[10]\n";
+
+if ( scalar(@array) != 11 ) 
+{die ("...error src/analysisClass.C is not a symbolic link");}
+
+## 2nd split
+my @codenameC = split(/\// , $array[10] );
+#print "@codenameC\n";
+#print "$codenameC[scalar(@codenameC)-1]\n";
+
+## 3rd split
+my ($codename,$EXT) = split(/\./ , $codenameC[scalar(@codenameC)-1] );
+#print "$codename\n";
+
+for $line(@inputList)
+{
+    chomp($line);
+
+    my @array1 = split(/\// , $line );
+    my ($dataset,$EXT) = split(/\./ , $array1[scalar(@array1)-1] );
+
+#    print "./main $line $cutfile $treename $outputDir/$codename\_\_\_$dataset $outputDir/$codename\_\_\_$dataset \n";
+#    system "./main $line $cutfile $treename $outputDir/$codename\_\_\_$dataset $outputDir/$codename\_\_\_$dataset";
+
+    
+    print "./scripts/submit_batch_ForSkimToCastor.py -i $line -c $cutfile -t $treename -o $outputDir/$codename\_\_\_$dataset -n $njobs -q $queue -d $fullCastorPath\n";
+    system "./scripts/submit_batch_ForSkimToCastor.py -i $line -c $cutfile -t $treename -o $outputDir/$codename\_\_\_$dataset -n $njobs -q $queue -d $fullCastorPath";    
+
+    time.sleep($wait);
+    
+}
+
+#---------------------------------------------------------#
+
+sub help(){
+    print "Usage: perl ./script/launchAnalysis.pl -i <inputList> -c <cutfile> -n <treename> -o <outputDir> -j <njobs> -q <queue> -d <castorDir> [-w <wait> -h <help?>] \n";
+    print "Example: perl scripts/launchAnalysis_batch.pl -i HeepStudies_v1/inputListAllCurrent.txt -c HeepStudies_v1/cutFile_HeepElectronStudiesV1.txt -n rootTupleTree/tree -o TestFrancesco -j 2 -q 1nh -w 5 -d LQ/Ntuples \n";
+    print "Options:\n";
+    print "-i <inputList>:      choose the file containing all the input lists for the analysis\n";
+    print "-n <treename>:       choose the name of the TTree of the .root files you want to analyze\n";
+    print "-c <cutfile>:        choose the name of the file with the analysis cuts\n";
+    print "-o <outputDir>:      choose the output directory where the .root files will be stored\n";
+    print "-j <njobs>:          choose number of jobs for batch submission, limited automatically to number of files in input list\n";
+    print "-q <queue>:          choose queue for batch submission (choose among cmst3 8nm 1nh 8nh 1nd 1nw)\n";
+    print "-w <wait>:           set a wait time in sec between submission of different datasets\n";
+    print "-d <castorDir>:      castor directory for the skim output (specify only the last part of the directory path, after $CASTOR_HOME)\n";
+    print "-h <yes> :           to print the help \n";
+    die "please, try again...\n";
+}
+
+# Perl trim function to remove whitespace from the start and end of the string
+sub trim($)
+{
+	my $string = shift;
+	$string =~ s/^\s+//;
+	$string =~ s/\s+$//;
+	return $string;
+}
---
+++ scripts/writeCommandsToRunOnMoreCutFiles_ForSkimToCastor.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# Please run this script from the rootNtupleAnalyzerV2 directory by:  
+# ./scripts/writeCommandsToRunOnMoreCutFiles.sh
+
+# This scripts creates the whole sets of commands needed to run the analysis on multiple cut files.
+# The commands will be written in a text file commandsToRunOnMoreCutFiles.txt in the current directory, 
+# to be used by doing cut&paste to a terminal.
+
+# Cut Files should first be created by a script ../rootNtupleMacrosV2/config/eejj/make_eejj_cutFiles.py
+# This script will then use those cut files to create the commands needed to run on them.
+
+#### INPUTS HERE ####
+#------------
+#files=`ls $LQMACRO/config/cutTable_enujjSample.txt` # list of cut files that will be used
+#files=`ls $LQMACRO/config/cutTable_enujjSample_QCD.txt` # list of cut files that will be used
+#files=`ls $LQMACRO/config/eejj/cutTable_eejjSample_*.txt $LQMACRO/config/cutTable_eejjSample.txt` # list of cut files that will be used
+#files=`ls $LQANA/Test_eejj_QCD/cutTable_eejjSample_QCD.txt` # list of cut files that will be used
+#files=`ls $LQANA/Test_enujj_QCD/cutTable_enujjSample_QCD.txt` # list of cut files that will be used
+files=`ls $LQMACRO2011/config/cutTable_enujjSample_2011_skim.txt` # list of cut files that will be used
+#------------
+OUTDIRPATH=$LQDATA  # a subdir will be created for each cut file 
+#SUBDIR=enujj_analysis/6.7pb-1_v2_Brussels_QCD_HLT20_noEle
+#SUBDIR=enujj_analysis/6.7pb-1_v2_Brussels_noDeltaPhi
+#SUBDIR=enujj_analysis/7.4pb-1_v1_QCD_HLT30
+#SUBDIR=enujj_analysis/15.1pb-1_v4
+#SUBDIR=enujj_analysis/21.9pb-1_v2
+SUBDIR=enujj_analysis/enujj-skim-ElectronHad-Run2011A-PromptReco-v4-AOD
+#SUBDIR=enujj_analysis/10.9pb-1_v3_EcalDeadCellsStudy
+         # output sub-directory (i.e. output will be in OUTDIRPATH/SUBDIR)
+         # it is suggested to specify the luminosity in the name of the directory
+#------------
+CASTORDIR=pippo/pluto/paperino
+FULLCASTORDIR=$CASTOR_HOME/$CASTORDIR #--> do not modify this line
+#------------
+ILUM=200 # integrated luminosity in pb-1 to be used for rescaling/merging MC samples
+FACTOR=1000 # numbers in final tables (but *not* in plots) will be multiplied by this scale factor (to see well the decimal digits)
+#------------
+CODENAME=analysisClass_enujjSample_2011 #the actual name of the code used to process the ntuples (without the suffix ".C") 
+#CODENAME=analysisClass_enujjSample_QCD #the actual name of the code used to process the ntuples (without the suffix ".C") 
+#CODENAME=analysisClass_eejjSample_QCD #the actual name of the code used to process the ntuples (without the suffix ".C") 
+#------------
+#INPUTLIST=config/PhotonSkim/inputListAllCurrent.txt #specify input list
+#INPUTLIST=config/ElectronSkim/inputListAllCurrent.txt #specify input list
+INPUTLIST=config/ElectronSkim/input_skim.txt
+#INPUTLIST=config/ElectronSkim/inputListAllCurrent_MC.txt #specify input list
+#------------
+XSECTION=config/xsection_7TeV_2011.txt #specify cross section file
+#XSECTION=config/xsection_7TeV_Zrescale_Wrescale.txt #specify cross section file
+#XSECTION=config/xsection_7TeV_Zrescale.txt #specify cross section file
+#------------
+#SAMPLELISTFORMERGING=config/sampleListForMerging_7TeV.txt #specify list for sample merging
+#SAMPLELISTFORMERGING=config/sampleListForMerging_7TeV_enujj_QCD.txt #specify list for sample merging
+SAMPLELISTFORMERGING=config/sampleListForMerging_7TeV_enujj_2011.txt #specify list for sample merging
+#------------
+##DATA
+NJOBS=20 #number of jobs for each dataset
+WAIT=5 #seconds of delay between submission of different datasets
+##MC
+#NJOBS=30 #number of jobs for each dataset
+#WAIT=5 #seconds of delay between submission of different datasets
+#------------
+QUEUE=1nh #bsub queue  
+#------------
+
+
+#### END OF INPUTS ####
+
+COMMANDFILE=commandsToRunOnMoreCutFiles_`hostname -s |perl -pi -e 's|lxplus[0-9]*|lxplus|'`.txt
+echo "" > $COMMANDFILE
+
+for file in $files
+do
+suffix=`basename $file`
+suffix=${suffix%\.*}
+cat >> $COMMANDFILE <<EOF
+
+####################################################
+#### launch, check and combine cmds for $suffix ####
+
+  ./scripts/launchAnalysis_batch_ForSkimToCastor.pl \
+    -i $INPUTLIST \
+    -n rootTupleTree/tree \
+    -c $file \
+    -o $OUTDIRPATH/$SUBDIR/output_$suffix  \
+    -j $NJOBS \
+    -q $QUEUE \
+    -w $WAIT \
+    -d $CASTORDIR \
+    | tee $OUTDIRPATH/$SUBDIR/output_$suffix/launch_${suffix}.log
+
+  ./scripts/check_combine_output_batch_ForSkimToCastor.py \
+    -i $INPUTLIST \
+    -c $CODENAME \
+    -d $OUTDIRPATH/$SUBDIR/output_$suffix \
+    -o $OUTDIRPATH/$SUBDIR/output_$suffix \
+    -q $QUEUE \
+    -s $FULLCASTORDIR \
+    | tee $OUTDIRPATH/$SUBDIR/output_$suffix/checkcombine_${suffix}.log
+
+EOF
+done
+
+
+echo "The set of commands to run on the cut files:" 
+for file in $files
+do
+echo "  " $file
+done 
+echo "has been written to $COMMANDFILE"
