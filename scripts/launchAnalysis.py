b4b6966dbcb1bf2d566d04a0c48efb8572ce15fc
eberry <>
2011-07-07 10:24:59 +0000

    Added functionality for multiprocessing

--- scripts/launchAnalysis.py
+++ scripts/launchAnalysis.py
@@ -7,16 +7,27 @@
 import optparse
 import sys
 import os
+import multiprocessing
+from multiprocessing import Pool
+
+#----------------------------------------------------------------
+# Define functions
+#----------------------------------------------------------------
+
+def printAndExecute ( command ): 
+    print command
+    os.system ( command ) 
 
 #----------------------------------------------------------------
 # Get user options
 #----------------------------------------------------------------
 
 parser = optparse.OptionParser(description='Launch the analysis')
-parser.add_option ("--input_list", "-i", dest = "input_list", type=str, help="Text file containing paths of input lists with .root files for analysis")
-parser.add_option ("--output_dir", "-o", dest = "output_dir", type=str, help="Directory where output will be stored" ) 
-parser.add_option ("--tree_name" , "-n", dest = "tree_name" , type=str, help="Name of TTree in .root files to be analyzed")
-parser.add_option ("--cut_file"  , "-c", dest = "cut_file"  , type=str, help="Text file containing cut values" ) 
+parser.add_option ("--input_list", "-i" , dest = "input_list", type=str, help="Text file containing paths of input lists with .root files for analysis")
+parser.add_option ("--output_dir", "-o" , dest = "output_dir", type=str, help="Directory where output will be stored" ) 
+parser.add_option ("--tree_name" , "-n" , dest = "tree_name" , type=str, help="Name of TTree in .root files to be analyzed")
+parser.add_option ("--cut_file"  , "-c" , dest = "cut_file"  , type=str, help="Text file containing cut values" ) 
+parser.add_option ("--ncores"    , "-p", dest = "ncores"     , type=int, help="Number of processor cores to be used to run the job" )
 
 (options, args) = parser.parse_args()
 
@@ -24,6 +35,10 @@ if not options.input_list : parser.print_help(); sys.exit()
 if not options.output_dir : parser.print_help(); sys.exit()
 if not options.tree_name  : parser.print_help(); sys.exit()
 if not options.cut_file   : parser.print_help(); sys.exit()
+
+ncores = multiprocessing.cpu_count()
+if options.ncores :
+    ncores = options.ncores
 
 #----------------------------------------------------------------
 # Bail if the cut file doesn't exist:
@@ -56,6 +71,7 @@ if not os.path.isdir ( options.output_dir ) :
 cut_file = open ( options.cut_file, "r" ) 
 found_json = False
 for line in cut_file:
+    line = line.split()
     if line[0:4] == "JSON":
 
         json_file_path = ""
@@ -94,15 +110,26 @@ else :
     code_name = os.path.realpath ( "src/analysisClass.C" ).split("/")[-1][:-2]
 
 #----------------------------------------------------------------
-# Now we can run the analysis
+# Loop over the input list and get a command for each file
 #----------------------------------------------------------------
 
 input_list = open ( options.input_list, "r" ) 
+command_list = []
+
 for line in input_list:
     dataset = line.strip().split("/")[-1][:-4]
     output_file_name = options.output_dir + "/" + code_name + "___" + dataset    
     command = "./main " + line.strip() + " " + options.cut_file + " " + options.tree_name + " " + output_file_name + " " + output_file_name
+    command_list.append ( command ) 
 
-    print command 
-    os.system ( command ) 
+#----------------------------------------------------------------
+# Execute using all available CPUs.  Quit on Ctrl-C.
+#----------------------------------------------------------------
 
+pool = Pool ( ncores ) 
+try:
+    pool.map_async( printAndExecute, command_list ).get(99999999)
+except KeyboardInterrupt:
+    print "\n\nCtrl-C detected: Bailing." 
+    pool.terminate()
+    sys.exit(1) 
