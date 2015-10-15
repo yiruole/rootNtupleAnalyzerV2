1f9c67791bedce59af9f02999bb479080f65a4f6
Seth Cooper <seth.cooper@cern.ch>
2015-10-15 14:56:42 +0200

    Update for running trigEff study.

--- scripts/writeCommandsToRunOnMoreCutFiles.sh
+++ scripts/writeCommandsToRunOnMoreCutFiles.sh
@@ -13,46 +13,38 @@
 #### INPUTS HERE ####
 #------------
 #files=`ls $LQMACRO/config/cutTable_enujjSample.txt` # list of cut files that will be used
-#files=`ls $LQMACRO/config/cutTable_enujjSample_QCD.txt` # list of cut files that will be used
-#files=`ls $LQMACRO/config/eejj/cutTable_eejjSample_*.txt $LQMACRO/config/cutTable_eejjSample.txt` # list of cut files that will be used
-#files=`ls $LQANA/Test_eejj_QCD/cutTable_eejjSample_QCD.txt` # list of cut files that will be used
-#files=`ls $LQANA/Test_enujj_QCD/cutTable_enujjSample_QCD.txt` # list of cut files that will be used
-files=`ls $LQMACRO2011/config/cutTable_enujjSample_2011.txt` # list of cut files that will be used
+files="/afs/cern.ch/work/s/scooper/private/cmssw/745/LQRootTupleMiniAOD745/src/Leptoquarks/macros/rootNtupleMacrosV2/config2015/Studies/SignalEffiTriggerStudy/cutTable_lq1_eejj_trigger1_id1_effiStudy.txt
+/afs/cern.ch/work/s/scooper/private/cmssw/745/LQRootTupleMiniAOD745/src/Leptoquarks/macros/rootNtupleMacrosV2/config2015/Studies/SignalEffiTriggerStudy/cutTable_lq1_eejj_trigger1_id2_effiStudy.txt
+/afs/cern.ch/work/s/scooper/private/cmssw/745/LQRootTupleMiniAOD745/src/Leptoquarks/macros/rootNtupleMacrosV2/config2015/Studies/SignalEffiTriggerStudy/cutTable_lq1_eejj_trigger1_id3_effiStudy.txt
+/afs/cern.ch/work/s/scooper/private/cmssw/745/LQRootTupleMiniAOD745/src/Leptoquarks/macros/rootNtupleMacrosV2/config2015/Studies/SignalEffiTriggerStudy/cutTable_lq1_eejj_trigger1_id4_effiStudy.txt
+/afs/cern.ch/work/s/scooper/private/cmssw/745/LQRootTupleMiniAOD745/src/Leptoquarks/macros/rootNtupleMacrosV2/config2015/Studies/SignalEffiTriggerStudy/cutTable_lq1_eejj_trigger1_id5_effiStudy.txt
+/afs/cern.ch/work/s/scooper/private/cmssw/745/LQRootTupleMiniAOD745/src/Leptoquarks/macros/rootNtupleMacrosV2/config2015/Studies/SignalEffiTriggerStudy/cutTable_lq1_eejj_trigger2_id1_effiStudy.txt
+/afs/cern.ch/work/s/scooper/private/cmssw/745/LQRootTupleMiniAOD745/src/Leptoquarks/macros/rootNtupleMacrosV2/config2015/Studies/SignalEffiTriggerStudy/cutTable_lq1_eejj_trigger3_id1_effiStudy.txt
+/afs/cern.ch/work/s/scooper/private/cmssw/745/LQRootTupleMiniAOD745/src/Leptoquarks/macros/rootNtupleMacrosV2/config2015/Studies/SignalEffiTriggerStudy/cutTable_lq1_eejj_trigger3_id2_effiStudy.txt
+/afs/cern.ch/work/s/scooper/private/cmssw/745/LQRootTupleMiniAOD745/src/Leptoquarks/macros/rootNtupleMacrosV2/config2015/Studies/SignalEffiTriggerStudy/cutTable_lq1_eejj_trigger3_id3_effiStudy.txt
+/afs/cern.ch/work/s/scooper/private/cmssw/745/LQRootTupleMiniAOD745/src/Leptoquarks/macros/rootNtupleMacrosV2/config2015/Studies/SignalEffiTriggerStudy/cutTable_lq1_eejj_trigger3_id4_effiStudy.txt
+/afs/cern.ch/work/s/scooper/private/cmssw/745/LQRootTupleMiniAOD745/src/Leptoquarks/macros/rootNtupleMacrosV2/config2015/Studies/SignalEffiTriggerStudy/cutTable_lq1_eejj_trigger3_id5_effiStudy.txt"
+
 #------------
 OUTDIRPATH=$LQDATA  # a subdir will be created for each cut file 
-#SUBDIR=enujj_analysis/6.7pb-1_v2_Brussels_QCD_HLT20_noEle
-#SUBDIR=enujj_analysis/6.7pb-1_v2_Brussels_noDeltaPhi
-#SUBDIR=enujj_analysis/7.4pb-1_v1_QCD_HLT30
-#SUBDIR=enujj_analysis/15.1pb-1_v4
-#SUBDIR=enujj_analysis/21.9pb-1_v2
-SUBDIR=enujj_analysis/330pb-1_enujjskim
-#SUBDIR=enujj_analysis/10.9pb-1_v3_EcalDeadCellsStudy
+SUBDIR=trigIDSpring15EffStudyEejjSignals
          # output sub-directory (i.e. output will be in OUTDIRPATH/SUBDIR)
          # it is suggested to specify the luminosity in the name of the directory
 #------------
 ILUM=330 # integrated luminosity in pb-1 to be used for rescaling/merging MC samples
 FACTOR=1000 # numbers in final tables (but *not* in plots) will be multiplied by this scale factor (to see well the decimal digits)
 #------------
-CODENAME=analysisClass_enujjSample_2011 #the actual name of the code used to process the ntuples (without the suffix ".C") 
-#CODENAME=analysisClass_enujjSample_QCD #the actual name of the code used to process the ntuples (without the suffix ".C") 
-#CODENAME=analysisClass_eejjSample_QCD #the actual name of the code used to process the ntuples (without the suffix ".C") 
+CODENAME=analysisClass_lq1_effiStudy #the actual name of the code used to process the ntuples (without the suffix ".C") 
 #------------
-#INPUTLIST=config/PhotonSkim/inputListAllCurrent.txt #specify input list
-INPUTLIST=config/ElectronSkimENUJJ/inputListAllCurrent.txt #specify input list
-#INPUTLIST=config/ElectronSkim/input_skim.txt
-#INPUTLIST=config/ElectronSkim/inputListAllCurrent_MC.txt #specify input list
+INPUTLIST=config/LQToUE_BetaOne_RunIIMC_MINIAODSIM/inputListAllCurrent.txt #specify input list
 #------------
 XSECTION=config/xsection_7TeV_2011.txt #specify cross section file
-#XSECTION=config/xsection_7TeV_Zrescale_Wrescale.txt #specify cross section file
-#XSECTION=config/xsection_7TeV_Zrescale.txt #specify cross section file
 #------------
-#SAMPLELISTFORMERGING=config/sampleListForMerging_7TeV.txt #specify list for sample merging
-#SAMPLELISTFORMERGING=config/sampleListForMerging_7TeV_enujj_QCD.txt #specify list for sample merging
 SAMPLELISTFORMERGING=config/sampleListForMerging_7TeV_enujj_2011.txt #specify list for sample merging
 #------------
 ##DATA
 NJOBS=5 #number of jobs for each dataset
-WAIT=5 #seconds of delay between submission of different datasets
+WAIT=1 #seconds of delay between submission of different datasets
 ##MC
 #NJOBS=30 #number of jobs for each dataset
 #WAIT=5 #seconds of delay between submission of different datasets
@@ -61,7 +53,7 @@ QUEUE=1nh #bsub queue
 
 #### END OF INPUTS ####
 
-COMMANDFILE=commandsToRunOnMoreCutFiles_`hostname -s |perl -pi -e 's|lxplus[0-9]*|lxplus|'`.txt
+COMMANDFILE=commandsToRunOnMoreCutFiles_trigEffStudyEejjSignals_trigsNewIDs_`hostname -s |perl -pi -e 's|lxplus[0-9]*|lxplus|'`.txt
 echo "" > $COMMANDFILE
 
 for file in $files
@@ -83,7 +75,7 @@ cat >> $COMMANDFILE <<EOF
     -w $WAIT \
     | tee $OUTDIRPATH/$SUBDIR/output_$suffix/launch_${suffix}.log
 
-  ./scripts/check_combine_output_batch.py \
+   rm ToBeResubmitted.list  && ./scripts/check_combine_output_batch.py \
     -i $INPUTLIST \
     -c $CODENAME \
     -d $OUTDIRPATH/$SUBDIR/output_$suffix \
