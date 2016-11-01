036b99a7d98c3c7d671390b4e77b94b798fe83c8
sethcooper <seth.cooper@cern.ch>
2016-11-01 17:11:08 +0100

    fix to support post-2015 data; cleanup

--- scripts/combineCommon.py
+++ scripts/combineCommon.py
@@ -33,7 +33,7 @@ def SanitizeDatasetNameFromFullDataset(dataset):
   #print 'SanitizeDatasetNameFromFullDataset: dataset looks like:'+dataset
   # this logic is somewhat copied from the submission script for the ntuples:
   #    https://github.com/CMSLQ/submitJobsWithCrabV2/blob/master/createAndSubmitJobsWithCrab3.py
-  if not 'Run2015' in dataset:
+  if not 'Run20' in dataset:
     outputFileNames = []
     outputFileNames.append(dataset[1:dataset.find('_Tune')])
     outputFileNames.append(dataset[1:dataset.find('_13TeV')])
@@ -53,8 +53,10 @@ def SanitizeDatasetNameFromFullDataset(dataset):
     elif 'amcatnloFXFX' in dataset:
       outputFile+='_amcatnloFXFX'
   else:
-    outputFile = dataset[1:].replace('/','__')
-    outputFile = outputFile.split('__')[0]+'__'+outputFile.split('__')[1]
+    #outputFile = dataset[1:].replace('/','__')
+    #outputFile = outputFile.split('__')[0]+'__'+outputFile.split('__')[1]
+    outputFile = dataset[1:].replace('/','_')
+    outputFile = outputFile.split('_')[0]+'_'+outputFile.split('_')[1]
   return outputFile
 
 
@@ -123,76 +125,13 @@ def lookupXSection(datasetNameFromInputList,xsectionDict):
   for dataset in xsectionDict.keys():
     if dataset.startswith(datasetNameFromInputList):
       return xsectionDict[dataset]
-  print 'ERROR: lookupXSection(): xsectionDict does not have an entry for',datasetNameFromInputList
+  #print
+  #print 'ERROR'
+  #for key,val in xsectionDict.iteritems():
+  #  print 'sample=',key,'xsection=',val
+  print 'ERROR: lookupXSection(): xsectionDict does not have an entry for',datasetNameFromInputList,'; i.e., no dataset in xsectionDict starts with this.'
   exit(-1)
-  #try:
-  #  #return xsectionDict[datasetNameFromInputList]
-  #except KeyError:
-  #  print
-  #  #for key,val in xsectionDict.iteritems():
-  #  #  print 'sample=',key,'xsection=',val
-  #  print 'ERROR: lookupXSection(): xsectionDict does not have an entry for',datasetNameFromInputList
-  #  exit(-1)
 
-
-#def lookupXSection(datasetFromAnalysis,xsectionFile):
-#  dataset_forXsec = datasetFromAnalysis
-#  if datasetFromAnalysis.endswith('_reduced_skim'):
-#    dataset_forXsec = datasetFromAnalysis[0:datasetFromAnalysis.find('_reduced_skim')]
-#
-#  xsectionDataset = ''
-#  xsectionVal = -999
-#  for lin1 in open( xsectionFile ):
-#
-#    lin1 = string.strip(lin1,"\n")
-#
-#    (dataset , xsection_val) = string.split(lin1)
-#    #print dataset + " " + xsection_val
-#
-#    dataset_mod_1 = dataset[1:].replace('/','__')
-#    #print dataset_mod_1 + " " + xsection_val
-#
-#    # TODO: fix this hack!
-#    if ( dataset_mod_1.startswith(dataset_forXsec+'_Tune') or
-#         dataset_mod_1.startswith(dataset_forXsec+'_13TeV') or
-#         #('Run20' in dataset_mod_1 and dataset_mod_1.startswith(dataset_forXsec)) or
-#         ('Run20' in dataset_mod_1 and dataset[1:].replace('/','_').startswith(dataset_forXsec)) or
-#         (dataset_forXsec=='DYJetsToLL_Mbin_M-50' and dataset_mod_1.startswith('DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8')) or
-#         (dataset_forXsec=='TTJets_SingleLeptFromTbar_ext1' and dataset_mod_1.startswith('TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2')) or
-#         (dataset_forXsec=='TTJets_DiLept_ext1' and dataset_mod_1.startswith('TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ext1-v1'))):
-#      # TODO: fix this hack
-#      # special handling of DYJetsToLL_M-50
-#      # for madgraph; Mbin has _Mbin in it
-#      if dataset_forXsec=='DYJetsToLL_M-50':
-#        if not 'madgraph' in dataset:
-#          continue
-#      #elif dataset_forXsec=='DYJetsToLL_Mbin_M-50':
-#      #  if not 'amcatnloFXFX' in dataset:
-#      #    continue
-#      #
-#      if len(xsectionDataset) <= 0:
-#        xsectionDataset = dataset
-#        xsectionVal = xsection_val
-#      elif xsectionVal != xsection_val:
-#        print 'ERROR: Two datasets in xsection file start with',dataset_forXsec
-#        print '1)',xsectionDataset,xsectionVal
-#        print '2)',dataset,xsection_val
-#        print 'Cannot figure out which is correct; exiting'
-#        sys.exit()
-#
-#  xsection_val = xsectionVal
-#  dataset = xsectionDataset
-#  if xsection_val < -1: # -1 being the value for data
-#    print "ERROR: xsection for dataset " + dataset_forXsec + " not found in " + xsectionFile
-#    print "Expected a line in xsection file to start with "+dataset_forXsec+"_Tune but couldn't find one."
-#    print "exiting..."
-#    sys.exit()
-#  return dataset,xsection_val
-#
-#
-
-#def AddHisto(inputHistoName, outputHisto, inputRootFileName, currentWeight,
-#             rebin=int(1), currentColor=int(1), currentFillStyle=int(1001), currentMarker=int(1)):
 
 def UpdateTable(inputTable, outputTable):
     if not outputTable:
