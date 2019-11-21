b613be1aac7c0e4241a197097af4b62f1c2a4c87
Seth Cooper <seth.cooper@cern.ch>
2019-11-21 17:50:34 +0100

    handle nanoAOD rounding for electron eta

--- src/QCDFakeRate.C
+++ src/QCDFakeRate.C
@@ -8,6 +8,7 @@ using namespace std;
 QCDFakeRate::QCDFakeRate(string qcdFilename)
 {
   TFile* qcdTFile = TFile::Open(qcdFilename.c_str());
+  // x-axis-->SCEt, y-axis-->|SCEta|
   //FIXME right now the graph name, etc. are hardcoded, fix
   TH2F* histoB = 0;
   qcdTFile->GetObject("Barrel_Fake_Rate",histoB);
@@ -29,45 +30,47 @@ float QCDFakeRate::GetFakeRate(const float& eta, const float& et, bool verbose)
 {
   float fr = -1.0;
   float etLookup = et;
-  if(et>=35)
-  {
-    // make sure that we don't ask for an Et that is beyond the max x range of the histogram
-    // NB: fix this by filling the overflow with the same bin content as the last bin
-    if(et > histoBarrel->GetXaxis()->GetXmax() || et > histoEndcap->GetXaxis()->GetXmax()) {
-      etLookup = min(histoBarrel->GetXaxis()->GetXmax(),histoEndcap->GetXaxis()->GetXmax())-1;
-      if(verbose) {
-        std::cout << "INFO: electron Et was: " << et << " which was beyond the bounds of the histogram: [barrel: " << histoBarrel->GetXaxis()->GetXmax()
-          << ", endcap: " << histoEndcap->GetXaxis()->GetXmax() << "]; changed it to " << etLookup << " for fake rate lookup." << std::endl;
-      }
-    }
-    if(fabs(eta) < 1.4442) {
-      fr = histoBarrel->GetBinContent(histoBarrel->GetBin(histoBarrel->GetXaxis()->FindBin(etLookup),1)); // 1 y bin
-      if(verbose) {
-        std::cout << "INFO: Found barrel electron with et=" << etLookup << " and eta=" << eta << "; FR=" << fr << std::endl;
-      }
-    }
-    else {
-      fr = histoEndcap->GetBinContent(histoEndcap->GetBin(histoEndcap->GetXaxis()->FindBin(etLookup),histoEndcap->GetYaxis()->FindBin(fabs(eta))));
-      if(verbose) {
-        std::cout << "INFO: Found endcap electron with et=" << etLookup << " and eta=" << eta << "; FR=" << fr << std::endl;
-      }
-    }
-  }
-  else if(et<35 && eta != 0) {
-    fr = 0.99;
-    if(verbose) {
-      std::cout << "INFO: Found electron with LOW et=" << etLookup << " and eta=" << eta << "; FR will be set to 0.99. FR=" << fr << std::endl;
-    }
-  }
-  else if(et==0 && eta==0) { // uninitialized/unstored electron2 (events with only 1 loose ele)
+  if(et==0 && eta==0) { // uninitialized/unstored electron2 (events with only 1 loose ele)
     fr = 0.99;
     if(verbose) {
       std::cout << "INFO: Found electron with zero et=" << etLookup << " and zero eta=" << eta << "; FR will be set to 0.99. FR=" << fr << std::endl;
     }
   }
-
+  else {
+    // make sure that we don't ask for an Et that is beyond the max x range of the histogram
+    // NB: fix this by filling the overflow with the same bin content as the last bin
+    if(et > histoBarrel->GetXaxis()->GetXmax() || et > histoEndcap->GetXaxis()->GetXmax()) {
+      etLookup = min(histoBarrel->GetXaxis()->GetXmax(),histoEndcap->GetXaxis()->GetXmax())-1;
+    } else if(et < 35) {
+      etLookup = 35;
+    }
+    if(verbose && etLookup != et) {
+      std::cout << "INFO: electron Et was: " << et << " which was beyond the bounds of the histogram: [barrel: " << histoBarrel->GetXaxis()->GetXmax()
+        << " - " << histoBarrel->GetXaxis()->GetXmin() << ", endcap: " << histoEndcap->GetXaxis()->GetXmax() << " - " << histoEndcap->GetXaxis()->GetXmin()
+        << "]; changed it to " << etLookup << " for fake rate lookup." << std::endl;
+    }
+    if(fabs(eta) < 1.5) { // add extra margin b/c nanoAOD and composite variable rounding; assumes eta cuts are applied upstream
+      fr = histoBarrel->GetBinContent(histoBarrel->GetBin(histoBarrel->GetXaxis()->FindBin(etLookup),1)); // 1 y bin (eta)
+      if(verbose) {
+        std::cout << "INFO: Found barrel electron with et=" << etLookup << " and eta=" << eta << "; FR=" << fr << std::endl;
+      }
+    }
+    else {
+      float etaLookup = fabs(eta);
+      // protect against eta < 1.566  or eta < 2.5 b/c nanoAOD and composite variable rounding; assumes eta cuts are applied upstream
+      if(fabs(etaLookup) >= histoEndcap->GetYaxis()->GetXmax())
+        etaLookup = histoEndcap->GetYaxis()->GetXmax()-0.01;
+      else if(fabs(etaLookup) < histoEndcap->GetYaxis()->GetXmin())
+        etaLookup = histoEndcap->GetYaxis()->GetXmin()+0.01;
+      fr = histoEndcap->GetBinContent(histoEndcap->GetBin(histoEndcap->GetXaxis()->FindBin(etLookup),histoEndcap->GetYaxis()->FindBin(etaLookup)));
+      if(verbose) {
+        std::cout << "INFO: Found endcap electron with et=" << etLookup << " and eta=" << eta << "; FR=" << fr << std::endl;
+      }
+    }
+  }
   if(fr<=0) {
     std::cerr << "ERROR: Found an electron with unknown fake rate: eta=" << eta << "; eT=" << et << "; return " << fr << " for qcd fake rate" << std::endl;
+    std::cerr << "ERROR: Found barrel eta bin: " << histoBarrel->GetYaxis()->FindBin(fabs(eta)) << "; found barrel et bin: " << histoBarrel->GetXaxis()->FindBin(etLookup) << std::endl;
     std::cerr << "ERROR: Found endcap eta bin: " << histoEndcap->GetYaxis()->FindBin(fabs(eta)) << "; found endcap et bin: " << histoEndcap->GetXaxis()->FindBin(etLookup) << std::endl;
   }
   assert(fr>0);
