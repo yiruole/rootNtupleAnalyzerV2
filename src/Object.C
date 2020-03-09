14672d001127b31cf220eb8735b35b73dbecc2f8
Seth Cooper <seth.cooper@cern.ch>
2020-03-09 08:53:15 +0100

    update for stockNano

--- src/Object.C
+++ src/Object.C
@@ -69,7 +69,10 @@ void Object::initP4() {
   m_eta = m_collection->ReadArrayBranch<Float_t>(name+"_eta",m_raw_index);
   m_phi = m_collection->ReadArrayBranch<Float_t>(name+"_phi",m_raw_index);
   if(name=="Muon")
+    // stock nano
     m_pt = m_collection->ReadArrayBranch<Float_t>("Muon_tunepRelPt", m_raw_index)*m_collection->ReadArrayBranch<Float_t>("Muon_pt", m_raw_index);
+    //// custom nano
+    //m_pt = m_collection->ReadArrayBranch<Float_t>("Muon_ptTuneP",m_raw_index);
   if(uncorrectPt)
     m_pt/=m_collection->ReadArrayBranch<Float_t>(name+"_eCorr",m_raw_index);
 }
