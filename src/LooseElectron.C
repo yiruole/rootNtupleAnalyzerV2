dc0f4dbcd3038c161a35be19e33c0e2f684c66cf
Seth Cooper <seth.cooper@cern.ch>
2020-04-06 09:36:12 +0200

    use miniaod pt for now for loose electrons (to be changed back to SCEt when NanoAODv7 is available

--- src/LooseElectron.C
+++ src/LooseElectron.C
@@ -9,7 +9,11 @@ LooseElectron::LooseElectron (Collection & c, unsigned short i, short j):
 }
 
 float & LooseElectron::Pt(){
-  scEt =  SCEnergy()/cosh(SCEta());
-  return scEt;
+  //scEt =  SCEnergy()/cosh(SCEta());
+  //return scEt;
+  //FIXME: go back to scEta when NanoAODv7 is out
+  float pt = m_collection->ReadArrayBranch<Float_t>("Electron_pt",m_raw_index);
+  ptCorr = pt/ECorr();
+  return ptCorr;
 } 
 
