7055384bf0dd9cc165102a5b25165490718d4347
Seth Cooper <seth.cooper@cern.ch>
2020-04-06 09:27:36 +0200

    update condor sub files

--- condor_submitRSKQCD.sh
+++ condor_submitRSKQCD.sh
@@ -4,4 +4,8 @@
 # aug28
 #python scripts/launchAnalysis_batch_ForSkimToEOS.py -i config/2016_custom2016NanoSkimNewNanoDataWithJson_eoscms/inputList_singlePhoton.txt -o /afs/cern.ch/user/s/scooper/work/private/data/Leptoquarks/nano/2016/rskQCD_28aug2019 -n Events -c /afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/ReducedSkims/cutTable_lq1_skim_QCD.txt -j 10 -q workday -d /eos/user/s/scooper/LQ/Nano/rskQCD_28aug2019 -r
 # oct8; new postProcSkim with Ele_pt > 35 GeV
-python scripts/launchAnalysis_batch_ForSkimToEOS.py -i config/2016_nanoPostProc_eoscms/inputList_singlePhoton.txt -o /afs/cern.ch/user/s/scooper/work/private/data/Leptoquarks/nano/2016/rskQCD_8oct2019 -n Events -c /afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/ReducedSkims/cutTable_lq1_skim_QCD.txt -j 10 -q workday -d /eos/user/s/scooper/LQ/Nano/rskQCD_8oct2019 -r
+#python scripts/launchAnalysis_batch_ForSkimToEOS.py -i config/2016_nanoPostProc_eoscms/inputList_singlePhoton.txt -o /afs/cern.ch/user/s/scooper/work/private/data/Leptoquarks/nano/2016/rskQCD_8oct2019 -n Events -c /afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/ReducedSkims/cutTable_lq1_skim_QCD.txt -j 10 -q workday -d /eos/user/s/scooper/LQ/Nano/rskQCD_8oct2019 -r
+SKIMNAME=rskQCD_29mar2020
+#INPUTLIST=config/nanoV6_2017_postProc/inputListAllCurrent.txt
+INPUTLIST=config/nanoV6_2017_postProc/resubQCDRSK.txt
+python scripts/launchAnalysis_batch_ForSkimToEOS.py -j 40 -q workday -i $INPUTLIST -o /afs/cern.ch/user/s/scooper/work/private/data/Leptoquarks/nanoV6/2017/$SKIMNAME -n Events -c /afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/ReducedSkims/cutTable_lq1_skim_QCD.txt -d /eos/user/s/scooper/LQ/NanoV6/$SKIMNAME -r
