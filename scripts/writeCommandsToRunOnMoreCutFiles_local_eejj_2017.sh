c18829ccd30dba9876a1985a3586463f55d528ec
Seth Cooper <seth.cooper@cern.ch>
2020-04-16 10:25:45 +0200

    add new script files for 2017/2018

---
+++ scripts/writeCommandsToRunOnMoreCutFiles_local_eejj_2017.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+# Please run this script from the rootNtupleAnalyzerV2 directory by:  
+# ./scripts/writeCommandsToRunOnMoreCutFiles.sh
+
+# This scripts creates the whole sets of commands needed to run the analysis on multiple cut files.
+# The commands will be written in a text file commandsToRunOnMoreCutFiles.txt in the current directory, 
+# to be used by doing cut&paste to a terminal.
+
+# Cut Files should first be created by a script ../rootNtupleMacrosV2/config/eejj/make_eejj_cutFiles.py
+# This script will then use those cut files to create the commands needed to run on them.
+
+#### INPUTS HERE ####
+#------------
+# analysis - FinalSels
+files="/afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2017/Analysis/cutTable_lq_eejj.txt"
+# opt
+#files="/afs/cern.ch/user/s/scooper/work/private/cmssw/8011/TestRootNTuplizerRecipe/src/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/Optimization/cutTable_lq_eejj_opt.txt"
+
+#files="/afs/cern.ch/user/s/scooper/work/private/cmssw/8011/TestRootNTuplizerRecipe/src/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/Analysis/cutTable_lq_eejj_noJets.txt"
+#files="/afs/cern.ch/user/s/scooper/work/private/cmssw/LQRootTuples7414/src/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/Analysis/cutTable_lq_eejj_loosenEleRequirements_2lowPtJets.txt"
+#files="/afs/cern.ch/user/s/scooper/work/private/cmssw/LQRootTuples7414/src/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/Analysis/cutTable_lq_eejj_eles50GeV_jets50GeV_st300GeV.txt"
+# studies
+#files="/afs/cern.ch/user/s/scooper/work/private/cmssw/LQRootTuples7414/src/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/Analysis/cutTable_lq_eejj_loosenEleRequirements_1Jet.txt"
+#files="/afs/cern.ch/user/s/scooper/work/private/cmssw/LQRootTuples7414/src/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/Analysis/cutTable_lq_eejj_loosenEleRequirements_noJetRequirement.txt"
+#files="/afs/cern.ch/user/s/scooper/work/private/cmssw/LQRootTuples7414/src/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/Analysis/cutTable_lq_eejj_1jetOrLess.txt"
+#files="/afs/cern.ch/user/s/scooper/work/private/cmssw/LQRootTuples7414/src/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/Analysis/cutTable_lq_eejj_preselectionOnly.txt
+#/afs/cern.ch/user/s/scooper/work/private/cmssw/LQRootTuples7414/src/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/Analysis/cutTable_lq_eejj_2012preselectionOnly.txt
+#/afs/cern.ch/user/s/scooper/work/private/cmssw/LQRootTuples7414/src/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/Analysis/cutTable_lq_eejj_preselectionOnly_tightenEleCuts.txt"
+#files="/afs/cern.ch/user/s/scooper/work/private/cmssw/LQRootTuples7414/src/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/Studies/SignalEffiTriggerStudy/cutTable_lq1_eejj_trigger5_id1_effiStudy.txt"
+#files="/afs/cern.ch/user/s/scooper/work/private/cmssw/745/LQRootTupleMiniAOD745/src/Leptoquarks/macros/rootNtupleMacrosV2/config2015/Analysis/cutTable_lq_eejj_preselectionOnly.txt"
+#files=`ls $LQMACRO/config2012/Analysis/cutTable_lq_eejj.txt`
+#------------
+OUTDIRPATH=$LQDATA  # a subdir will be created for each cut file 
+SUBDIR=nanoV6/2017/analysis/eejj_attempt_1apr
+
+# output sub-directory (i.e. output will be in OUTDIRPATH/SUBDIR)
+# it is suggested to specify the luminosity in the name of the directory
+#------------
+ILUM=41540 #FIXME: this number is just from the Egamma twiki
+FACTOR=1000 # numbers in final tables (but *not* in plots) will be multiplied by this scale factor (to see well the decimal digits)
+#------------
+EXE=main
+CODENAME=analysisClass_lq_eejj
+#------------
+INPUTLIST=config/nanoV6_2017_pskEEJJ_31mar2020/inputListAllCurrent.txt
+#------------
+XSECTION=config/xsection_13TeV_2015.txt #specify cross section file
+#------------
+SAMPLELISTFORMERGING=config/sampleListForMerging_13TeV_eejj_2017.txt
+#------------
+#NCORES=8 #Number of processor cores to be used to run the job
+NCORES=10 #Number of processor cores to be used to run the job
+#------------
+
+#### END OF INPUTS ####
+
+COMMANDFILE=commandsToRunOnMoreCutFiles_eejj_2017_local_`hostname -s`.txt
+#COMMANDFILE=commandsToRunOnMoreCutFiles_eejj_optimization_local_`hostname -s`.txt
+
+#COMMANDFILE=commandsToRunOnMoreCutFiles_eejj_heep61_local_`hostname -s`.txt
+#COMMANDFILE=commandsToRunOnMoreCutFiles_eejj_noJets_local_`hostname -s`.txt
+#COMMANDFILE=commandsToRunOnMoreCutFiles_eejj_trigEff_reHLT_AK5_local_`hostname -s`.txt
+#COMMANDFILE=commandsToRunOnMoreCutFiles_eejj_AK5_local_`hostname -s`.txt
+#COMMANDFILE=commandsToRunOnMoreCutFiles_eejj_AK4CHS_local_`hostname -s`.txt
+echo "" > $COMMANDFILE
+
+for file in $files
+do
+suffix=`basename $file`
+suffix=${suffix%\.*}
+cat >> $COMMANDFILE <<EOF
+
+####################################################
+#### launch, check and combine cmds for $suffix ####
+
+time python scripts/launchAnalysis.py \
+    -e $EXE \
+    -k $CODENAME \
+    -i $INPUTLIST \
+    -n rootTupleTree/tree \
+    -c $file \
+    -o $OUTDIRPATH/$SUBDIR/output_$suffix  \
+    -p $NCORES \
+    >& launch_${suffix}_`hostname -s`.log
+
+mv launch_${suffix}_`hostname -s`.log $OUTDIRPATH/$SUBDIR/output_$suffix/
+
+time  ./scripts/combinePlots.py \
+    -i $INPUTLIST \
+    -c $CODENAME \
+    -d $OUTDIRPATH/$SUBDIR/output_$suffix \
+    -l  `echo "$ILUM*$FACTOR" | bc` \
+    -x $XSECTION  \
+    -o $OUTDIRPATH/$SUBDIR/output_$suffix \
+    -s $SAMPLELISTFORMERGING \
+    -f $OUTDIRPATH/$SUBDIR/output_$suffix/launch_${suffix}_`hostname -s`.log \
+    | tee $OUTDIRPATH/$SUBDIR/output_$suffix/combineTablesAndPlots_${suffix}.log
+
+EOF
+done
+
+
+echo "The set of commands to run on the cut files:" 
+for file in $files
+do
+echo "  " $file
+done 
+echo "has been written to $COMMANDFILE"
