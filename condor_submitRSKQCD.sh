3f2cd2c64611722847db60a0382354705d2e2e4a
Seth Cooper <seth.cooper@cern.ch>
2020-05-12 14:07:54 +0200

    latest submit script updates

--- condor_submitRSKQCD.sh
+++ condor_submitRSKQCD.sh
@@ -7,8 +7,8 @@
 #python scripts/launchAnalysis_batch_ForSkimToEOS.py -i config/2016_nanoPostProc_eoscms/inputList_singlePhoton.txt -o /afs/cern.ch/user/s/scooper/work/private/data/Leptoquarks/nano/2016/rskQCD_8oct2019 -n Events -c /afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/ReducedSkims/cutTable_lq1_skim_QCD.txt -j 10 -q workday -d /eos/user/s/scooper/LQ/Nano/rskQCD_8oct2019 -r
 #SKIMNAME=rskQCD_29mar2020
 #INPUTLIST=config/nanoV6_2017_postProc/inputListAllCurrent.txt
-#python scripts/launchAnalysis_batch_ForSkimToEOS.py -j 40 -q workday -i $INPUTLIST -o /afs/cern.ch/user/s/scooper/work/private/data/Leptoquarks/nanoV6/2017/$SKIMNAME -n Events -c /afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/ReducedSkims/cutTable_lq1_skim_QCD.txt -d /eos/user/s/scooper/LQ/NanoV6/2017/$SKIMNAME -r
+#python scripts/launchAnalysis_batch_ForSkimToEOS.py -j 40 -q workday -i $INPUTLIST -o /afs/cern.ch/user/s/scooper/work/private/data/Leptoquarks/nanoV6/2017/skims/$SKIMNAME -n Events -c /afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2015/ReducedSkims/cutTable_lq1_skim_QCD.txt -d /eos/user/s/scooper/LQ/NanoV6/2017/skims/$SKIMNAME -r
 # 2018
-SKIMNAME=rskQCD_15apr2020
+SKIMNAME=rskQCD_8may2020
 INPUTLIST=config/nanoV6_2018_postProc/inputListAllCurrent.txt
-python scripts/launchAnalysis_batch_ForSkimToEOS.py -j 40 -q workday -i $INPUTLIST -o /afs/cern.ch/user/s/scooper/work/private/data/Leptoquarks/nanoV6/2018/$SKIMNAME -n Events -c /afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2018/ReducedSkims/cutTable_lq1_skim_QCD.txt -d /eos/user/s/scooper/LQ/NanoV6/2018/$SKIMNAME -r
+python scripts/launchAnalysis_batch_ForSkimToEOS.py -j 40 -q workday -i $INPUTLIST -o /afs/cern.ch/user/s/scooper/work/private/data/Leptoquarks/nanoV6/2018/skims/$SKIMNAME -n Events -c /afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2018/ReducedSkims/cutTable_lq1_skim_QCD.txt -d /eos/user/s/scooper/LQ/NanoV6/2018/skims/$SKIMNAME -r
