7e131fb75aec0e61030a80d84590188dbdc7d2ee
Seth Cooper <seth.cooper@cern.ch>
2020-05-12 14:03:11 +0200

    add possibility not to do DBS query

--- scripts/genXsec_cfg.py
+++ scripts/genXsec_cfg.py
@@ -5,7 +5,7 @@ from dbs.exceptions.dbsClientException import dbsClientException
 
 options = VarParsing ('analysis')
 options.register ('dataset',
-                  'test',
+                  'default',
                   VarParsing.multiplicity.singleton,
                   VarParsing.varType.string,
                   "Full name/path of dataset")
@@ -13,53 +13,64 @@ options.register ('dataset',
 options.parseArguments()
 
 datasetpath = options.dataset
-if datasetpath=='test':
-    print 'ERROR: must specify full dataset name of NANOAODSIM dataset: dataset=/Primary/Secondary/NANOAODSIM'
+if datasetpath=='default':
+    print 'ERROR: must specify full dataset name of NANOAODSIM dataset: dataset=/Primary/Secondary/NANOAODSIM or dataset=test to skip DBS querying'
     print 'Exiting.'
     print
     exit(-1)
-print 'DBS: query for parent of dataset='+datasetpath+'...',
 
-try:
-    api = DbsApi(url = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/')
-    parents = api.listDatasetParents(dataset=datasetpath)
-except dbsClientException, ex:
-    print
-    print "Caught API Exception %s: %s "  % (ex.name,ex)
-    exit(-1)
+doQuery = True
+if datasetpath=='test':
+    doQuery = False
 
-#for parent in parents:
-#    print parent
-if len(parents) > 1:
-    print
-    print 'ERROR: got multiple parents for dataset:'
-    for parent in parents:
-        print parent['parent_dataset']
-    print 'ERROR: not sure how to continue.'
-    exit(-2)
+if doQuery:
+    print 'DBS: query for parent of dataset='+datasetpath+'...',
+    
+    try:
+        api = DbsApi(url = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/')
+        parents = api.listDatasetParents(dataset=datasetpath)
+    except dbsClientException, ex:
+        print
+        print "Caught API Exception %s: %s "  % (ex.name,ex)
+        exit(-1)
+    
+    #for parent in parents:
+    #    print parent
+    if len(parents) > 1:
+        print
+        print 'ERROR: got multiple parents for dataset:'
+        for parent in parents:
+            print parent['parent_dataset']
+        print 'ERROR: not sure how to continue.'
+        exit(-2)
+    
+    print 'Done'
+    parentDataset = parents[0]['parent_dataset']
+    print 'DBS: Found parent dataset='+parentDataset
+    print 'DBS: query for files dataset...',
+    
+    try:
+        api = DbsApi(url = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/')
+        #dbsFiles = api.listFiles(dataset=datasetpath)
+        dbsFiles = api.listFiles(dataset=parentDataset)
+    except dbsClientException, ex:
+        print
+        print "Caught API Exception %s: %s "  % (ex.name,ex)
+        exit(-3)
+    
+    print 'Done'
+    #for dbsFile in dbsFiles:
+    #    print dbsFile['logical_file_name']
+    # get first N LFNs
+    filesToUse = 200
+    firstNFiles = [dbsFile['logical_file_name'] for dbsFile in dbsFiles]
+    if len(firstNFiles) > filesToUse:
+        firstNFiles = firstNFiles[-1*filesToUse:]
+    #print firstNFiles
+    print 'INFO: using last',len(firstNFiles),'out of',len(dbsFiles),'files in parent dataset as input to GenXSecAnalyzer'
+else:
+    print "INFO: not doing DBS query."
 
-print 'Done'
-parentDataset = parents[0]['parent_dataset']
-print 'DBS: Found parent dataset='+parentDataset
-print 'DBS: query for files dataset...',
-
-try:
-    api = DbsApi(url = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/')
-    #dbsFiles = api.listFiles(dataset=datasetpath)
-    dbsFiles = api.listFiles(dataset=parentDataset)
-except dbsClientException, ex:
-    print
-    print "Caught API Exception %s: %s "  % (ex.name,ex)
-    exit(-3)
-
-print 'Done'
-#for dbsFile in dbsFiles:
-#    print dbsFile['logical_file_name']
-# get first ten LFNs
-firstTenFiles = [dbsFile['logical_file_name'] for dbsFile in dbsFiles]
-if len(firstTenFiles) > 10:
-    firstTenFiles = firstTenFiles[:10]
-print 'INFO: using first',len(firstTenFiles),'out of',len(dbsFiles),'files in parent dataset as input to GenXSecAnalyzer'
 print 'Run.'
 print
 
@@ -72,27 +83,20 @@ process.maxEvents = cms.untracked.PSet(
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 
-maxEvents = cms.untracked.PSet(input = cms.untracked.int32(5000000) )
+maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1) )
 secFiles = cms.untracked.vstring() 
-process.source = cms.Source ("PoolSource",
-        fileNames = cms.untracked.vstring(firstTenFiles),
-        #fileNames = cms.untracked.vstring(options.inputFiles), 
-        #fileNames = cms.untracked.vstring(
-
-        #    '/store/mc/RunIISummer16MiniAODv2/GJets_Pt-20To100_13TeV-sherpa/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/F87154C7-C4CC-E611-BB3D-5065F3819221.root',
-        #    '/store/mc/RunIISummer16MiniAODv2/GJets_Pt-20To100_13TeV-sherpa/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/5051D169-93CA-E611-8E9D-70106F4D2638.root',
-        #    '/store/mc/RunIISummer16MiniAODv2/GJets_Pt-20To100_13TeV-sherpa/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/48EF5369-A3CA-E611-AEC2-00266CFFC51C.root',
-        #    '/store/mc/RunIISummer16MiniAODv2/GJets_Pt-20To100_13TeV-sherpa/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/B405219B-A9CA-E611-83AC-1866DAEB5C74.root',
-        #    '/store/mc/RunIISummer16MiniAODv2/GJets_Pt-20To100_13TeV-sherpa/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/CA4241ED-ACCA-E611-9CD1-002590E7D5A6.root',
-        #    '/store/mc/RunIISummer16MiniAODv2/GJets_Pt-20To100_13TeV-sherpa/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/62F771CE-C4CC-E611-BA3B-C4346BC076D0.root',
-        #    '/store/mc/RunIISummer16MiniAODv2/GJets_Pt-20To100_13TeV-sherpa/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/50CB01C0-C7CC-E611-802E-002590E7D7EA.root',
-        #    '/store/mc/RunIISummer16MiniAODv2/GJets_Pt-20To100_13TeV-sherpa/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/E0B49C85-89CC-E611-AC60-00259073E382.root',
-        #    '/store/mc/RunIISummer16MiniAODv2/GJets_Pt-20To100_13TeV-sherpa/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/AC6EE796-C5CC-E611-B281-00259074AE7A.root',
-        #    '/store/mc/RunIISummer16MiniAODv2/GJets_Pt-20To100_13TeV-sherpa/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/120000/20560A41-C0CC-E611-BCA8-0CC47A537688.root',
-
-        #  ),
-
+if doQuery:
+    process.source = cms.Source ("PoolSource",
+            fileNames = cms.untracked.vstring(firstNFiles),
             secondaryFileNames = secFiles)
+else:
+    process.source = cms.Source ("PoolSource",
+            fileNames = cms.untracked.vstring(
+                # can put filenames here manually
+                '/store/mc/RunIISummer16MiniAODv2/GJets_Pt-20To100_13TeV-sherpa/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/70000/F87154C7-C4CC-E611-BB3D-5065F3819221.root',
+              ),
+    )
+
 process.xsec = cms.EDAnalyzer("GenXSecAnalyzer")
 
 process.ana = cms.Path(process.xsec)
