cc503f05e1f0e6daa06a9cd4812ed9dedf2566a5
Seth Cooper <seth.cooper@cern.ch>
2020-06-24 17:14:55 +0200

    going with these fits for now

--- scripts/fitFakeRates.py
+++ scripts/fitFakeRates.py
@@ -11,8 +11,8 @@ funcTypes = ["pol0", "pol1", "pol2", "pol3"]
 fitColors = [kSpring-1, kRed+1, kAzure+1, kOrange+8]
 fitStyles = [1, 2]
 
-# years = [2016, 2017, 2018]
-years = [2016]
+allYears = [2016, 2017, 2018]
+years = [2018]  # years to fit at once
 
 fileNames = {}
 fileNames[2016] = "$LQANA/versionsOfFakeRate/2016/may25/plots.root"
@@ -41,14 +41,14 @@ fitRanges[2018]["End1_noHEM_post319077_2Jet"] = [[0, 175], [150, 1000]]
 fitRanges[2018]["End2_noHEM_post319077_2Jet"] = [[0, 175], [150, 1000]]
 fitRanges[2018]["Bar_HEMonly_post319077_2Jet"] = [[0, 225], [150, 1000]]
 fitRanges[2018]["End1_HEMonly_post319077_2Jet"] = [[0, 175], [150, 1000]]
-fitRanges[2018]["End2_HEMonly_post319077_2Jet"] = [[0, 175], [150, 1000]]
+fitRanges[2018]["End2_HEMonly_post319077_2Jet"] = [[0, 170], [100, 1000]]
 
 # use this after we've determined the function selection to write the combined function to the file
 # number of functions has to match number of specified fit ranges/regions
 # list: [functionType1, functionType2, splitPoint, functionType2, functionType3, splitPoint, etc.]
 selectedFits = {}
-selectedFits[2016] = {}
-for year in years:
+for year in allYears:
+    selectedFits[year] = {}
     for reg in regionsDict[year]:
         selectedFits[year][reg] = {}
 selectedFits[2016]["Bar_2Jet"]["funcs"] = ["pol2", "pol1", 150]
@@ -57,10 +57,31 @@ selectedFits[2016]["End2_2Jet"]["funcs"] = ["pol2", "pol2", 160]
 selectedFits[2016]["Bar_2Jet"]["splits"] = [150]
 selectedFits[2016]["End1_2Jet"]["splits"] = [140]
 selectedFits[2016]["End2_2Jet"]["splits"] = [160]
-if len(selectedFits) > 0:
-    writeFitResults = True
-else:
-    writeFitResults = False
+selectedFits[2017]["Bar_2Jet"]["funcs"] = ["pol2", "pol1"]
+selectedFits[2017]["End1_2Jet"]["funcs"] = ["pol2", "pol2"]
+selectedFits[2017]["End2_2Jet"]["funcs"] = ["pol2", "pol1"]
+selectedFits[2017]["Bar_2Jet"]["splits"] = [150]
+selectedFits[2017]["End1_2Jet"]["splits"] = [135]
+selectedFits[2017]["End2_2Jet"]["splits"] = [150]
+selectedFits[2018]["Bar_pre319077_2Jet"]["funcs"] = ["pol1", "pol2"]
+selectedFits[2018]["End1_pre319077_2Jet"]["funcs"] = ["pol2", "pol1"]
+selectedFits[2018]["End2_pre319077_2Jet"]["funcs"] = ["pol1", "pol0"]
+selectedFits[2018]["Bar_noHEM_post319077_2Jet"]["funcs"] = ["pol2", "pol1"]
+selectedFits[2018]["End1_noHEM_post319077_2Jet"]["funcs"] = ["pol1", "pol1"]
+selectedFits[2018]["End2_noHEM_post319077_2Jet"]["funcs"] = ["pol1", "pol0"]
+selectedFits[2018]["Bar_HEMonly_post319077_2Jet"]["funcs"] = ["pol1", "pol1"]
+selectedFits[2018]["End1_HEMonly_post319077_2Jet"]["funcs"] = ["pol2", "pol1"]
+selectedFits[2018]["End2_HEMonly_post319077_2Jet"]["funcs"] = ["pol2", "pol1"]
+selectedFits[2018]["Bar_pre319077_2Jet"]["splits"] = [193]
+selectedFits[2018]["End1_pre319077_2Jet"]["splits"] = [187.5]
+selectedFits[2018]["End2_pre319077_2Jet"]["splits"] = [220]
+selectedFits[2018]["Bar_noHEM_post319077_2Jet"]["splits"] = [150]
+selectedFits[2018]["End1_noHEM_post319077_2Jet"]["splits"] = [150]
+selectedFits[2018]["End2_noHEM_post319077_2Jet"]["splits"] = [134]
+selectedFits[2018]["Bar_HEMonly_post319077_2Jet"]["splits"] = [200]
+selectedFits[2018]["End1_HEMonly_post319077_2Jet"]["splits"] = [131]
+selectedFits[2018]["End2_HEMonly_post319077_2Jet"]["splits"] = [115]
+writeFitResults = True
 
 graphName = "fr{}_template"
 
@@ -164,17 +185,25 @@ if not os.path.isdir(pdf_folder) and pdf_folder != "":
 for can in canvases:
     can.Draw()
     can.Write()
-    can.Print(pdf_folder + "/" + can.GetName() + "_fit.pdf")
+    can.Print(pdf_folder + "/" + can.GetName() + "_fits.pdf")
 for func in functions:
     func.Write()
 
 # write manually-selected full functions
 if writeFitResults:
     for year in years:
+        thisFile = tfiles[year]
         for region in regionsDict[year]:
+            if len(selectedFits[year][region]) <= 0:
+                print "WARN: No selectedFits for year={} region={} specified; cannot write fit results".format(year, region)
+                continue
             selectedFitList = selectedFits[year][region]["funcs"]
             selectedFitSplitList = selectedFits[year][region]["splits"]
+            graph = thisFile.Get(graphName.format(region))
             canName = "y"+str(year)+"_"+region
+            canvas = TCanvas(canName+"_combinedFitCanv", canName+"_combinedFitCanv")
+            canvas.cd()
+            graph.Draw("ap")
             selectedFitFuncs = []
             selectedFuncTypes = []
             for frIndex, fitRange in enumerate(fitRanges[year][region]):
@@ -189,20 +218,39 @@ if writeFitResults:
             paramIndex = 0
             params = []
             formula = ""
+            prevSplitPoint = 0
             for i, func in enumerate(selectedFitFuncs):
                 formula += selectedFuncTypes[i]+"("+str(paramIndex)+")"
                 if i < len(selectedFitFuncs)-1:
-                    splitPoint = str(selectedFitSplitList[i])
+                    splitPoint = selectedFitSplitList[i]
                     formula += "*(x<"+str(splitPoint)+")+(x>="+str(splitPoint)+")*"
+                    func.SetRange(prevSplitPoint, splitPoint)  # for drawing
+                    prevSplitPoint = splitPoint
+                else:
+                    func.SetRange(prevSplitPoint, 5000)
+                func.SetLineWidth(2)
+                func.SetNpx(2000)
+                func.SetLineStyle(1)
+                func.Draw("same")
                 paramIndex += func.GetNpar()
                 params.extend([func.GetParameter(j) for j in range(0, func.GetNpar())])
             # nParams = sum([func.GetNpar() for func in selectedFitFuncs])
             print "Create TF1 with combined formula: '{}'".format(formula)
+            outputTFile.cd()
             combinedFitFunc = TF1(canName+"_combinedFit", formula)
             combinedFitFunc.SetRange(0, 5000)
             for i, param in enumerate(params):
                 combinedFitFunc.SetParameter(i, param)
+            combinedFitFunc.SetNpx(2000)
             combinedFitFunc.Write()
+            # combinedFitFunc.SetLineStyle(2)
+            # combinedFitFunc.SetLineWidth(1)
+            # combinedFitFunc.SetLineColor(kOrange+2)
+            # combinedFitFunc.Draw("same")
+            canvas.Modified()
+            canvas.Update()
+            canvas.Write()
+            canvas.Print(pdf_folder + "/" + canvas.GetName() + ".pdf")
 
 
 outputTFile.Close()
