f900beaee97243b2872ae32faa1fb06f850939c9
Seth Cooper <seth.cooper@cern.ch>
2020-08-20 09:46:20 +0200

    format, add verbose, don't check data datasets

--- scripts/check2017PU.py
+++ scripts/check2017PU.py
@@ -1,18 +1,26 @@
 from optparse import OptionParser
-import sys
 
-from dbs.apis.dbsClient import DbsApi
-from dbs.exceptions.dbsClientException import dbsClientException
+try:
+    import CRABClient
+    from dbs.apis.dbsClient import DbsApi
+    from dbs.exceptions.dbsClientException import dbsClientException
+except ImportError:
+    print
+    print "ERROR: Could not load dbs APIs.  Please source the crab3 setup:"
+    # print "source /cvmfs/cms.cern.ch/crab3/crab.sh"
+    print("source /cvmfs/cms.cern.ch/common/crab-setup.sh")
+    exit(-1)
 
 
 def GetParents(dataset):
     try:
-        api = DbsApi(url = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/')
+        api = DbsApi(url='https://cmsweb.cern.ch/dbs/prod/global/DBSReader/')
         parents = api.listDatasetParents(dataset=dataset)
     except dbsClientException, ex:
-        print "Caught API Exception %s: %s "  % (ex.name,ex)
+        print "Caught API Exception %s: %s " % (ex.name, ex)
         exit(-1)
     return parents
+
 
 def CheckForMultipleParents(parents):
     if len(parents) == 1:
@@ -49,6 +57,15 @@ parser.add_option(
     metavar="FILENAME",
     default=None,
 )
+parser.add_option(
+    "-v",
+    "--verbose",
+    dest="verbose",
+    help="verbose output",
+    metavar="VERBOSE",
+    default=False,
+    action="store_true",
+)
 
 (options, args) = parser.parse_args()
 
@@ -56,16 +73,17 @@ if options.datasets is None and options.filename is None:
     print 'ERROR: must specify dataset with -d or --dataset or filename with -f or --file'
     exit(-1)
 
+verbose = options.verbose
+
 if options.datasets is not None:
     datasetpath = options.datasets
-    #print datasetpath
     if ',' in datasetpath:
         datasetList = datasetpath.split(',')
     else:
         datasetList = [datasetpath]
 else:
     datasetList = []
-    with open(options.filename,"r") as theFile:
+    with open(options.filename, "r") as theFile:
         for line in theFile:
             split = line.split()
             if len(split) <= 0:
@@ -77,24 +95,29 @@ else:
             datasetList.append(dataset)
 
 for dataset in datasetList:
-    # print 'Examining dataset:',dataset,
-    print dataset,
+    if "NANOAODSIM" not in dataset:
+        continue  # no sense to check data
+    if not verbose:
+        print dataset,
+    else:
+        print "Examining dataset='"+dataset+"'"
     parents = GetParents(dataset)
     if not CheckForMultipleParents(parents):
         continue
-    
+
     parentDataset = parents[0]['parent_dataset']
-    # print '\tparent dataset='+parentDataset
-    
+    if verbose:
+        print '\tparent dataset='+parentDataset
+
     nextParents = GetParents(parentDataset)
     if not CheckForMultipleParents(nextParents):
         continue
-    
+
     parentOfParent = nextParents[0]['parent_dataset']
-    # print '\tparent of parent dataset=',parentOfParent
-    
+    if verbose:
+        print '\tparent of parent dataset=', parentOfParent
+
     if 'PU2017' in parentOfParent:
         print '\tPU OK'
     else:
         print '\tWrong PU was used in AOD dataset!'
-
