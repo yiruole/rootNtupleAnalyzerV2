63e2e4064b01c4efb684fbbe01dd4efd3f0a6087
Seth Cooper <seth.cooper@cern.ch>
2021-03-01 17:30:40 +0100

    add code to also open up the root file

--- scripts/ReadTTreePrint.py
+++ scripts/ReadTTreePrint.py
@@ -2,8 +2,24 @@
 
 import operator
 from prettytable import PrettyTable
+import sys
+import os
+import ROOT as r
 
+treeFilename = "root://eoscms.cern.ch//store/group/phys_exotica/leptonsPlusJets/LQ/scooper/nanoPostProc/2016/nanoAODv7/DYJetsToLL_Pt-650ToInf_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/LQ-v1/200803_164045/0000/DYJetsToLL_Pt-650ToInf_ext2_1.root"
 filename = "/tmp/scooper/treeLogTest.txt"
+numberOfLargestBranchesToShow = 50
+
+save = os.dup(sys.stdout.fileno())
+newout = file(filename, "w")
+os.dup2(newout.fileno(), sys.stdout.fileno())
+tfile = r.TFile.Open(treeFilename)
+tree = tfile.Get("Events")
+tree.Print()
+tfile.Close()
+os.dup2(save, sys.stdout.fileno())
+newout.close()
+
 branchNameToSizeDict = {}
 with open(filename, "r") as theFile:
     currentBranchName = ""
@@ -17,15 +33,15 @@ with open(filename, "r") as theFile:
 # sort
 sortedList = sorted(branchNameToSizeDict.items(), key=operator.itemgetter(1), reverse=True)
 totalSize = sum([thisSize for name, thisSize in sortedList])
-sortedList = sortedList[:100]  # limit number of items to 100
+sortedList = sortedList[:numberOfLargestBranchesToShow]  # limit number of items to 100
 
 # print
 print "total file size:", totalSize, "(MB)"
-print "space used by 100 largest branches:"
+print "space used by {} largest branches:".format(numberOfLargestBranchesToShow)
 t = PrettyTable(["Branch name", "size (MB)", "% of total"])
 t.float_format = "4.3"
 t.align["Branch name"] = "l"
 t.align["Size"] = "r"
 for brNameSize in sortedList:
-    t.add_row([brNameSize[0], brNameSize[1], brNameSize[1]/(1.0*totalSize)])
+    t.add_row([brNameSize[0], brNameSize[1], 100*brNameSize[1]/(1.0*totalSize)])
 print t
