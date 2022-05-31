c10cd09ec12d71d44d7507dde4e938709cde96d2
Seth Cooper <seth.cooper@cern.ch>
2022-05-31 15:55:31 +0200

    adjustments for EGM loose ID

--- scripts/fitFakeRates.py
+++ scripts/fitFakeRates.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 from ROOT import gROOT, gMinuit, gStyle, gPad, TFile, TF1, TCanvas, TLegend, kSpring, kAzure, kRed, kOrange
 
@@ -9,13 +11,15 @@ pdf_folder = "pdf"
 # fitFunctions = [TF1("pol0", "pol0"), TF1("pol1", "pol1"), TF1("pol2", "pol2")]
 funcTypes = ["pol0", "pol1", "pol2", "pol3"]
 fitColors = [kSpring-1, kRed+1, kAzure+1, kOrange+8]
-fitStyles = [1, 2]
+fitStyles = [1, 2, 2]
 
 allYears = [2016, 2017, 2018]
-years = [2018]  # years to fit at once
+years = [2016]  # years to fit at once
 
 fileNames = {}
-fileNames[2016] = "$LQANA/versionsOfFakeRate/2016/may25/plots.root"
+# fileNames[2016] = "$LQANA/versionsOfFakeRate/2016/may25/plots.root"
+# fileNames[2016] = "$LQANA/versionsOfFakeRate/2016/egammaLooseID/addFracFitToPlot/plots.root"
+fileNames[2016] = "$LQANA/versionsOfFakeRate/2016/mar21_egLoose/plots.root"
 fileNames[2017] = "$LQANA/versionsOfFakeRate/2017/apr17/plots.root"
 fileNames[2018] = "$LQANA/versionsOfFakeRate/2018/jun4/plots.root"
 
@@ -28,9 +32,10 @@ regionsDict[2018].extend(["Bar_HEMonly_post319077_2Jet", "End1_HEMonly_post31907
 
 fitRanges = {}
 fitRanges[2016] = {}
-fitRanges[2016]["Bar_2Jet"] = [[0, 200], [150, 1000]]
-fitRanges[2016]["End1_2Jet"] = [[0, 175], [150, 1000]]
-fitRanges[2016]["End2_2Jet"] = [[0, 175], [150, 1000]]
+# fitRanges[2016]["Bar_2Jet"] = [[0, 200], [150, 1000]]
+fitRanges[2016]["Bar_2Jet"] = [[0, 100], [100, 400], [400, 1000]]
+fitRanges[2016]["End1_2Jet"] = [[0, 500], [500, 1000]]
+fitRanges[2016]["End2_2Jet"] = [[50, 1000]]
 fitRanges[2017] = fitRanges[2016]
 fitRanges[2018] = {}
 fitRanges[2018]["Bar_pre319077_2Jet"] = [[0, 200], [150, 1000]]
@@ -44,19 +49,18 @@ fitRanges[2018]["End1_HEMonly_post319077_2Jet"] = [[0, 175], [150, 1000]]
 fitRanges[2018]["End2_HEMonly_post319077_2Jet"] = [[0, 170], [100, 1000]]
 
 # use this after we've determined the function selection to write the combined function to the file
-# number of functions has to match number of specified fit ranges/regions
-# list: [functionType1, functionType2, splitPoint, functionType2, functionType3, splitPoint, etc.]
+# number of functions has to match number of specified fit ranges/regions: 1 split per pair of functions
 selectedFits = {}
 for year in allYears:
     selectedFits[year] = {}
     for reg in regionsDict[year]:
         selectedFits[year][reg] = {}
-selectedFits[2016]["Bar_2Jet"]["funcs"] = ["pol2", "pol1", 150]
-selectedFits[2016]["End1_2Jet"]["funcs"] = ["pol2", "pol2", 140]
-selectedFits[2016]["End2_2Jet"]["funcs"] = ["pol2", "pol2", 160]
-selectedFits[2016]["Bar_2Jet"]["splits"] = [150]
-selectedFits[2016]["End1_2Jet"]["splits"] = [140]
-selectedFits[2016]["End2_2Jet"]["splits"] = [160]
+selectedFits[2016]["Bar_2Jet"]["funcs"] = ["pol0", "pol1", "pol0"]
+selectedFits[2016]["End1_2Jet"]["funcs"] = ["pol2", "pol0"]
+selectedFits[2016]["End2_2Jet"]["funcs"] = ["pol1"]
+selectedFits[2016]["Bar_2Jet"]["splits"] = [100, 400]
+selectedFits[2016]["End1_2Jet"]["splits"] = [500]
+selectedFits[2016]["End2_2Jet"]["splits"] = []
 selectedFits[2017]["Bar_2Jet"]["funcs"] = ["pol2", "pol1"]
 selectedFits[2017]["End1_2Jet"]["funcs"] = ["pol2", "pol2"]
 selectedFits[2017]["End2_2Jet"]["funcs"] = ["pol2", "pol1"]
@@ -83,7 +87,8 @@ selectedFits[2018]["End1_HEMonly_post319077_2Jet"]["splits"] = [131]
 selectedFits[2018]["End2_HEMonly_post319077_2Jet"]["splits"] = [115]
 writeFitResults = True
 
-graphName = "fr{}_template"
+# graphName = "fr{}_dataDrivenRatio"  # [was fr{}_template]
+graphName = "fr{}_dataDrivenFracFit"
 
 tfiles = {}
 for year in years:
@@ -98,16 +103,26 @@ for year in years:
     thisFile = tfiles[year]
     fitResults[year] = {}
     for region in regionsDict[year]:
+        isBarrel = False
+        if "bar" in region.lower():
+            isBarrel = True
         fitResults[year][region] = {}
         graph = thisFile.Get(graphName.format(region))
+        if not graph:
+            raise RuntimeError("Could not get graph named: {} from input file {}".format(graphName.format(region), thisFile))
         canName = "y"+str(year)+"_"+region
         canvas = TCanvas(canName, canName)
         canvas.cd()
         leg = TLegend(0.38, 0.71, 0.63, 0.88)
         leg.SetBorderSize(0)
         gPad.cd()
+        graph.SetMarkerColor(1)
+        graph.SetLineColor(1)
         graph.Draw("ap0")
-        graph.GetYaxis().SetRangeUser(0, 0.1)
+        if isBarrel:
+            graph.GetYaxis().SetRangeUser(0, 0.1)
+        else:
+            graph.GetYaxis().SetRangeUser(0, 0.3)
         if year != 2018:
             graph.SetTitle(str(year)+" FR, "+graph.GetTitle())
         gStyle.SetOptFit(0)
@@ -121,14 +136,18 @@ for year in years:
                 func.SetLineColor(fitColors[funcIndex])
                 func.SetRange(float(fitRange[0]), float(fitRange[1]))
                 func.SetLineStyle(fitStyles[frIndex])
-                print "Fit for year={} region={} fitRange={} funcType={}".format(year, region, fitRange, funcType)
+                print("Fit for year={} region={} fitRange={} funcType={}".format(year, region, fitRange, funcType))
                 graph.Fit(func, "WQRN", "", float(fitRange[0]), float(fitRange[1]))
                 result = graph.Fit(func, "SFRM", "", float(fitRange[0]), float(fitRange[1]))
-                # print "Func name: {} Chi2={} NDF={}; chi2/ndf={}".format(
-                #         func.GetName(), func.GetChisquare(), func.GetNDF(), func.GetChisquare()/func.GetNDF())
-                thisChi2OverNdf = result.Chi2()/result.Ndf()
+                print("Func name: {} Chi2={} NDF={}; chi2/ndf={}".format(
+                        func.GetName(), func.GetChisquare(), func.GetNDF(), func.GetChisquare()/func.GetNDF() if func.GetNDF() != 0 else "N/A"))
+                if func.GetNDF() == 0:
+                    print("ERROR: Fit for function:", func.GetName(), "resulted in NDF==0; skipping")
+                    fitResults[year][region][fitRangeStr][func] = [None]
+                    continue
                 if "CONVERGED" in gMinuit.fCstatu or "OK" in gMinuit.fCstatu:
                     # print "func: {} fit converged!".format(func.GetName())
+                    thisChi2OverNdf = result.Chi2()/result.Ndf()
                     if thisChi2OverNdf < thisFitRangeMinChi2OverNdf:
                         thisFitRangeMinChi2OverNdf = thisChi2OverNdf
                         # thisFitRangeBestFitResult = result
@@ -138,19 +157,19 @@ for year in years:
                     # print
                     fitResults[year][region][fitRangeStr][func] = [result]
                 else:
-                    print "ERROR: Fit for function:", func.GetName(), "did not converge; skipping"
+                    print("ERROR: Fit for function:", func.GetName(), "did not converge; skipping")
                     fitResults[year][region][fitRangeStr][func] = [None]
                     continue
                 canvas.cd()
                 gPad.cd()
                 func.Draw("same")
                 functions.append(func)
-            for func in fitResults[year][region][fitRangeStr].keys():
+            for func in list(fitResults[year][region][fitRangeStr].keys()):
                 if func == thisFitRangeBestFunc:
                     fitResults[year][region][fitRangeStr][func].append(True)
                 else:
                     fitResults[year][region][fitRangeStr][func].append(False)
-        for func in sorted(fitResults[year][region].values()[0].keys(), key=lambda x: len(x.GetExpFormula().Data())):
+        for func in sorted(list(fitResults[year][region].values())[0].keys(), key=lambda x: len(x.GetExpFormula().Data())):
             leg.AddEntry(func, func.GetExpFormula().Data(), "l")
         leg.Draw()
         canvas.Modified()
@@ -158,24 +177,24 @@ for year in years:
         canvases.append(canvas)
         legends.append(leg)
 
-    print
-    print "SUMMARY"
-    print
+    print()
+    print("SUMMARY")
+    print()
     for region in regionsDict[year]:
-        for fitRange in fitResults[year][region].keys():
-            print "Year={}, region={}, Fit range: {}".format(year, region, fitRange)
-            for func in fitResults[year][region][fitRange].keys():
+        for fitRange in list(fitResults[year][region].keys()):
+            print("Year={}, region={}, Fit range: {}".format(year, region, fitRange))
+            for func in list(fitResults[year][region][fitRange].keys()):
                 thisFitResult = fitResults[year][region][fitRange][func][0]
                 if thisFitResult is not None:
-                    print "--> func name: {} Chi2={} NDF={}; chi2/ndf={}".format(
+                    print("--> func name: {} Chi2={} NDF={}; chi2/ndf={}".format(
                             func.GetName(), thisFitResult.Chi2(), thisFitResult.Ndf(),
-                            thisFitResult.Chi2()/thisFitResult.Ndf()),
+                            thisFitResult.Chi2()/thisFitResult.Ndf()), end=' ')
                     if fitResults[year][region][fitRange][func][1]:
-                        print "[BEST]"
+                        print("[BEST]")
                     else:
-                        print
+                        print()
                 else:
-                    print "--> func name: {} fit did not converge.".format(func.GetName())
+                    print("--> func name: {} fit did not converge.".format(func.GetName()))
 
 outputTFile = TFile(outputFileName, "recreate")
 outputTFile.cd()
@@ -195,7 +214,7 @@ if writeFitResults:
         thisFile = tfiles[year]
         for region in regionsDict[year]:
             if len(selectedFits[year][region]) <= 0:
-                print "WARN: No selectedFits for year={} region={} specified; cannot write fit results".format(year, region)
+                print("WARN: No selectedFits for year={} region={} specified; cannot write fit results".format(year, region))
                 continue
             selectedFitList = selectedFits[year][region]["funcs"]
             selectedFitSplitList = selectedFits[year][region]["splits"]
@@ -235,7 +254,7 @@ if writeFitResults:
                 paramIndex += func.GetNpar()
                 params.extend([func.GetParameter(j) for j in range(0, func.GetNpar())])
             # nParams = sum([func.GetNpar() for func in selectedFitFuncs])
-            print "Create TF1 with combined formula: '{}'".format(formula)
+            print("Create TF1 with combined formula: '{}'".format(formula))
             outputTFile.cd()
             combinedFitFunc = TF1(canName+"_combinedFit", formula)
             combinedFitFunc.SetRange(0, 5000)
@@ -255,5 +274,5 @@ if writeFitResults:
 
 outputTFile.Close()
 
-for f in tfiles.values():
+for f in list(tfiles.values()):
     f.Close()
