49827dff25d4a47793e84ae429c45b333f23f1f0
Seth Cooper <seth.cooper@cern.ch>
2022-05-31 15:54:25 +0200

    add separate presel scripts

--- scripts/condor_submitEEJJPresel.sh
+++ scripts/condor_submitEEJJPresel.sh
@@ -7,19 +7,21 @@ if [ "$YEAR" = "2016" ]; then
   echo "Doing 2016!"
   #INPUTLIST=config/nanoV7_2016_rskSingleEleL_9apr2021/inputListAllCurrent.txt
   # INPUTLIST=config/nanoV7_2016_rskSingleEleL_9apr2021/inputList_wjetPt50Only.txt
-  INPUTLIST=config/nanoV7_2016_rskSingleEleL_egLoose_21sep2021/inputListAllCurrent.txt
-  SKIMNAME=pskEEJJ_egLoose_22sep2021
-  CUTFILE=/afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2016/FlatNTupleSkims/cutTable_lq_eejjPreselection_skim.txt
+  #INPUTLIST=config/nanoV7_2016_rskSingleEleL_egLoose_21sep2021/inputListAllCurrent.txt
+  #INPUTLIST=config/nanoV7_2016_rskSingleEleL_egLoose_31jan2022/inputListAllCurrent.txt
+  INPUTLIST=config/nanoV7_2016_rskSingleEleL_egLoose_3feb2022/inputListAllCurrent.txt
+  SKIMNAME=pskEEJJ_egLoose_4feb2022
+  CUTFILE=/afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2016/FlatNTupleSkims/cutTable_lq_eejj_preselection_skim.txt
 elif [ "$YEAR" = "2017" ]; then
   #INPUTLIST=config/nanoV7_2017_rskSingleEleL_9apr2021/inputListAllCurrent.txt
   INPUTLIST=config/nanoV7_2017_rskSingleEleL_9apr2021/inputList_jun10.txt
   SKIMNAME=pskEEJJ_12apr2021
-  CUTFILE=/afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2017/FlatNTupleSkims/cutTable_lq_eejjPreselection_skim.txt
+  CUTFILE=/afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2017/FlatNTupleSkims/cutTable_lq_eejj_preselection_skim.txt
 elif [ "$YEAR" = "2018" ]; then
   #INPUTLIST=config/nanoV7_2018_rskSingleEleL_9apr2021/inputListAllCurrent.txt
   INPUTLIST=config/nanoV7_2018_rskSingleEleL_9apr2021/inputList_jun10.txt
   SKIMNAME=pskEEJJ_12apr2021
-  CUTFILE=/afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2017/FlatNTupleSkims/cutTable_lq_eejjPreselection_skim.txt
+  CUTFILE=/afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2017/FlatNTupleSkims/cutTable_lq_eejj_preselection_skim.txt
 else
   echo "ERROR: did not understand given year of '$YEAR' which is not one of 2016, 2017, 2018"
   echo "Usage: $0 [2016 | 2017 | 2018]"
---
+++ scripts/condor_submitQCDEEJJPresel.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+YEAR=$1
+
+if [ "$YEAR" = "2016" ]; then
+  echo "Doing 2016!"
+  #INPUTLIST=config/nanoV7_2016_rskQCD_looseEGM_23mar2022_comb/inputListAllCurrent.txt
+  INPUTLIST=config/nanoV7_2016_rskQCD_heep_4apr2022_comb/inputListAllCurrent.txt
+  SKIMNAME=pskQCDEEJJ_heep_6apr2022
+  #CUTFILE=/afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2016/FlatNTupleSkims/cutTable_lq_QCD_eejj_preselection_skim.txt
+  CUTFILE=/tmp/scooper/cutTable_lq_QCD_eejj_preselection_skim.txt
+elif [ "$YEAR" = "2017" ]; then
+  #INPUTLIST=config/nanoV7_2017_rskSingleEleL_9apr2021/inputListAllCurrent.txt
+  INPUTLIST=config/nanoV7_2017_rskSingleEleL_9apr2021/inputList_jun10.txt
+  SKIMNAME=pskQCDEEJJ_12apr2021
+  CUTFILE=/afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2017/FlatNTupleSkims/cutTable_lq_QCD_eejj_preselection_skim.txt
+elif [ "$YEAR" = "2018" ]; then
+  #INPUTLIST=config/nanoV7_2018_rskSingleEleL_9apr2021/inputListAllCurrent.txt
+  INPUTLIST=config/nanoV7_2018_rskSingleEleL_9apr2021/inputList_jun10.txt
+  SKIMNAME=pskQCDEEJJ_12apr2021
+  CUTFILE=/afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2017/FlatNTupleSkims/cutTable_lq_QCD_eejj_preselection_skim.txt
+else
+  echo "ERROR: did not understand given year of '$YEAR' which is not one of 2016, 2017, 2018"
+  echo "Usage: $0 [2016 | 2017 | 2018]"
+  exit -1
+fi
+
+EOSDIR=/eos/cms/store/user/scooper/LQ/NanoV7/skims/${YEAR}/$SKIMNAME
+#EOSDIR=/eos/cms/store/group/phys_exotica/leptonsPlusJets/LQ/scooper/nanoV7/skims/${YEAR}/$SKIMNAME
+#EOSDIR=/eos/user/s/scooper/LQ/NanoV7/skims/${YEAR}/$SKIMNAME
+OUTPUTDIR=/afs/cern.ch/user/s/scooper/work/private/data/Leptoquarks/nanoV7/skims/${YEAR}/$SKIMNAME
+
+python scripts/launchAnalysis_batch_ForSkimToEOS.py -i $INPUTLIST -o $OUTPUTDIR -c $CUTFILE -q microcentury -d $EOSDIR -j 5 -n rootTupleTree/tree
+#python scripts/launchAnalysis_batch_ForSkimToEOS.py -i $INPUTLIST -o $OUTPUTDIR -c $CUTFILE -q longlunch -d $EOSDIR -j 10 -n rootTupleTree/tree
