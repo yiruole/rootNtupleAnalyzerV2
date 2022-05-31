903034ca39bb342159cac217995ba2ba40a72c24
Seth Cooper <seth.cooper@cern.ch>
2022-05-31 15:49:00 +0200

    move helper functions to class

--- include/QCDFakeRate.h
+++ include/QCDFakeRate.h
@@ -10,6 +10,8 @@ class QCDFakeRate {
     QCDFakeRate();
     QCDFakeRate(std::string filename, std::vector<std::string>& regionVec, int year);
     float GetFakeRate(double et, std::string regionName);
+    static std::string GetFakeRateRegion(bool isBarrel, bool isEndcap1, bool isEndcap2, float eta=-999, float phi=-999, int run=-1);
+    static bool isHEMElectron(float eta, float phi);
 
   private:
     std::vector<std::unique_ptr<TF1> > funcVec;
--- src/QCDFakeRate.C
+++ src/QCDFakeRate.C
@@ -18,3 +18,31 @@ QCDFakeRate::QCDFakeRate(std::string filename, std::vector<std::string>& regionV
 float QCDFakeRate::GetFakeRate(double et, std::string regionName) {
   return funcVec[regionToFuncIndexMap[regionName]]->Eval(et);
 }
+
+bool QCDFakeRate::isHEMElectron(float eta, float phi) {
+  if(eta <= -1.3 && eta >= -3.0)
+    if(phi <= -0.87 && phi >= -1.57)
+      return true;
+  return false;
+}
+
+std::string QCDFakeRate::GetFakeRateRegion(bool isBarrel, bool isEndcap1, bool isEndcap2, float eta, float phi, int run) {
+  std::string region = "";
+  if(isBarrel)
+    region = "Bar_";
+  else if(isEndcap1)
+    region = "End1_";
+  else if(isEndcap2)
+    region = "End2_";
+  
+  if(run < 0)
+    return region+"2Jet";
+  else { // 2018
+    if(run < 319077)
+      return region+"pre319077_2Jet";
+    else if(isHEMElectron(eta, phi))
+      return region+"HEMonly_post319077_2Jet";
+    else
+      return region+"noHEM_post319077_2Jet";
+  }
+}
