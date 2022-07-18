c595227dec682910936cc174438ca551aa04caff
Seth Cooper <seth.cooper@cern.ch>
2022-07-18 15:45:45 +0200

    update for UL2016pre/post

--- scripts/condor_submitSEleL.sh
+++ scripts/condor_submitSEleL.sh
@@ -2,26 +2,31 @@
 
 YEAR=$1
 
-if [ "$YEAR" = "2016" ]; then
-  echo "Doing 2016!"
-  SKIMNAME=rskSingleEleL_29jun2022
-  #INPUTLIST=config/nanoV7_2016_postProc/inputListAllCurrent.txt
-  INPUTLIST=config/UL16NanoV9_nanoSkim_29jun2022/inputListAllCurrent.txt
+if [ "$YEAR" = "2016preVFP" ]; then
+  echo "Doing 2016preVFP!"
+  SKIMNAME=rskSingleEleL_15jul2022
+  INPUTLIST=config/UL16preVFP_nanoV9_nanoSkim_29jun2022/inputListAllCurrent.txt
+  CUTFILE=/afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2016/ReducedSkims/preVFP/cutTable_lq1_skim_SingleEle_loose.txt
+elif [ "$YEAR" = "2016postVFP" ]; then
+  echo "Doing 2016postVFP!"
+  SKIMNAME=rskSingleEleL_15jul2022
+  INPUTLIST=config/UL16postVFP_nanoV9_nanoSkim_29jun2022/inputListAllCurrent.txt
+  CUTFILE=/afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config2016/ReducedSkims/postVFP/cutTable_lq1_skim_SingleEle_loose.txt
 elif [ "$YEAR" = "2017" ]; then
-  SKIMNAME=rskSingleEleL_9apr2021
-  #INPUTLIST=config/nanoV7_2017_postProc/inputListAllCurrent.txt
-  INPUTLIST=config/nanoV7_2017_postProc/inputList_jun10.txt
+  SKIMNAME=rskSingleEleL_7jul2022
+  INPUTLIST=config/UL17NanoV9_nanoSkim_1jul2022/inputListAllCurrent.txt
+  CUTFILE=/afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config${YEAR}/ReducedSkims/cutTable_lq1_skim_SingleEle_loose.txt
 elif [ "$YEAR" = "2018" ]; then
   SKIMNAME=rskSingleEleL_9apr2021
   #INPUTLIST=config/nanoV7_2018_postProc/inputListAllCurrent.txt
   INPUTLIST=config/nanoV7_2018_postProc/inputList_jun10.txt
+  CUTFILE=/afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config${YEAR}/ReducedSkims/cutTable_lq1_skim_SingleEle_loose.txt
 else
-  echo "ERROR: did not understand given year of '$YEAR' which is not one of 2016, 2017, 2018"
-  echo "Usage: $0 [2016 | 2017 | 2018]"
+  echo "ERROR: did not understand given year of '$YEAR' which is not one of 2016preVFP, 2016postVFP, 2017, 2018"
+  echo "Usage: $0 [2016preVFP | 2016postVFP | 2017 | 2018]"
   exit -1
 fi
 
-CUTFILE=/afs/cern.ch/user/s/scooper/work/private/LQNanoAODAttempt/Leptoquarks/analyzer/rootNtupleMacrosV2/config${YEAR}/ReducedSkims/cutTable_lq1_skim_SingleEle_loose.txt
 #EOSDIR=/eos/user/s/scooper/LQ/NanoV7/skims/${YEAR}/$SKIMNAME
 #EOSDIR=/eos/cms/store/user/scooper/LQ/NanoV7/skims/${YEAR}/$SKIMNAME
 EOSDIR=/eos/cms/store/group/phys_exotica/leptonsPlusJets/LQ/scooper/ultralegacy/skims/${YEAR}/$SKIMNAME
@@ -30,5 +35,5 @@ OUTPUTDIR=/afs/cern.ch/user/s/scooper/work/private/data/Leptoquarks/ultralegacy/
 #python scripts/launchAnalysis_batch_ForSkimToEOS.py -j 20 -q tomorrow -i $INPUTLIST -o $OUTPUTDIR -n Events -c $CUTFILE -d $EOSDIR -r
 #python scripts/launchAnalysis_batch_ForSkimToEOS.py -j 50 -q testmatch -i $INPUTLIST -o $OUTPUTDIR -n Events -c $CUTFILE -d $EOSDIR -r
 # below for nanoSkims
-python scripts/launchAnalysis_batch_ForSkimToEOS.py -j 20 -q testmatch -i $INPUTLIST -o $OUTPUTDIR -n rootTupleTree/tree -c $CUTFILE -d $EOSDIR -r
+python3 scripts/launchAnalysis_batch_ForSkimToEOS.py -j 50 -q testmatch -i $INPUTLIST -o $OUTPUTDIR -n rootTupleTree/tree -c $CUTFILE -d $EOSDIR -r
 # FIXME do we still need the testmatch queue?
