2d2d5af41d692a15e4d60030270280ea1abdb5c4
Seth Cooper <seth.cooper@cern.ch>
2023-03-15 10:47:31 +0100

    update for UL

--- src/ElectronScaleFactors.C
+++ src/ElectronScaleFactors.C
@@ -1,39 +1,51 @@
 #include <math.h>
 #include <algorithm>
-// from https://twiki.cern.ch/twiki/bin/view/CMS/EgammaIDRecipesRun2#Efficiencies_and_scale_factors
-// and https://indico.cern.ch/event/604912/contributions/2490008/attachments/1418885/2173433/HEEP7_Moriond_GSFix_final_RW_v3.pdf [slide 17]
-// and https://indico.cern.ch/event/831669/contributions/3485543/attachments/1871797/3084930/ApprovalSlides_EE_v3.pdf [slide 10]
-class ElectronScaleFactors2016 {
+#include <string>
+#include <stdexcept>
+// UL
+// https://indico.cern.ch/event/1259725/contributions/5294821/attachments/2603712/4496433/HEEP%20ID%202016UL%20Update%20for%20EGamma.pdf
+class ElectronScaleFactors2016preVFP {
 
   public:
     static double LookupHeepSF(const double scEta) {
       if(fabs(scEta)<1.566)
-        return 0.983;
+        return 0.989;
       else if(fabs(scEta)<2.5)
-        return 0.991;
+        return 0.992;
       return -1.;
     }
 
-    // this is the prompt uncertainty
     static double LookupHeepSFSyst(const double scEta, const double et) {
-      if(fabs(scEta)<1.566) {
-        if(et < 90)
-          return 0.01;
-        else
-          return 0.01*std::min(1+(et-90)*0.0022,3.0);
-      }
-      else if(fabs(scEta)<2.5) {
-        if(et < 90)
-          return 0.01;
-        else
-          return 0.01*std::min(1+(et-90)*0.0143,4.0);
-      }
+      if(fabs(scEta)<1.566)
+        return 0.0051;
+      else if(fabs(scEta)<2.5)
+        return 0.0076;
       return -1.;
     }
 
 };
 
-// see: https://twiki.cern.ch/twiki/bin/view/CMS/Egamma2017DataRecommendations#2017_Efficiency_Scale_Factors
+class ElectronScaleFactors2016postVFP {
+
+  public:
+    static double LookupHeepSF(const double scEta) {
+      if(fabs(scEta)<1.566)
+        return 0.98;
+      else if(fabs(scEta)<2.5)
+        return 0.989;
+      return -1.;
+    }
+
+    static double LookupHeepSFSyst(const double scEta, const double et) {
+      if(fabs(scEta)<1.566)
+        return 0.0051;
+      else if(fabs(scEta)<2.5)
+        return 0.0081;
+      return -1.;
+    }
+
+};
+
 class ElectronScaleFactors2017 {
 
   public:
@@ -42,25 +54,17 @@ class ElectronScaleFactors2017 {
     static double LookupHeepSF(const double scEta)
     {
       if(fabs(scEta)<1.566)
-        return 0.968;  // from the slides: https://indico.cern.ch/event/831669/contributions/3485543/attachments/1871797/3084930/ApprovalSlides_EE_v3.pdf
+        return 0.979;
       else if(fabs(scEta)<2.5)
-        return 0.973;
+        return 0.987;
       return -1.;
     }
 
     static double LookupHeepSFSyst(const double scEta, const double et) {
-      if(fabs(scEta)<1.566) {
-        if(et < 90)
-          return 0.01;
-        else
-          return 0.01*std::min(1+(et-90)*0.0022,3.0);
-      }
-      else if(fabs(scEta)<2.5) {
-        if(et < 90)
-          return 0.02;
-        else
-          return 0.01*std::min(1+(et-90)*0.0143,5.0);
-      }
+      if(fabs(scEta)<1.566)
+        return 0.0051;
+      else if(fabs(scEta)<2.5)
+        return 0.01;
       return -1.;
     }
 };
@@ -68,42 +72,47 @@ class ElectronScaleFactors2017 {
 
 class ElectronScaleFactors2018 {
   public:
-    // for HEEPv7.0-2018Prompt 
     static double LookupHeepSF(const double scEta)
     {
       if(fabs(scEta)<1.566)
-        return 0.969;
+        return 0.973;
       else if(fabs(scEta)<2.5)
-        return 0.984;
+        return 0.98;
       return -1.;
     }
 
     static double LookupHeepSFSyst(const double scEta, const double et) {
-      return ElectronScaleFactors2017::LookupHeepSFSyst(scEta, et);
+      if(fabs(scEta)<1.566)
+        return 0.0041;
+      else if(fabs(scEta)<2.5)
+        return 0.0112;
+      return -1.;
     }
 };
 
 class ElectronScaleFactorsRunII {
   public:
-    static double LookupHeepSF(const double scEta, const int analysisYear) {
-      if(analysisYear==2016)
-        return ElectronScaleFactors2016::LookupHeepSF(scEta);
-      else if(analysisYear==2017)
+    static double LookupHeepSF(const double scEta, const std::string analysisYear) {
+      if(analysisYear=="2016preVFP")
+        return ElectronScaleFactors2016preVFP::LookupHeepSF(scEta);
+      else if(analysisYear=="2016postVFP")
+        return ElectronScaleFactors2016postVFP::LookupHeepSF(scEta);
+      else if(analysisYear=="2017")
         return ElectronScaleFactors2017::LookupHeepSF(scEta);
-      else if(analysisYear==2018)
+      else if(analysisYear=="2018")
         return ElectronScaleFactors2018::LookupHeepSF(scEta);
-      else
-        return -1;
+      throw std::runtime_error("Did not understand year given: "+analysisYear);
     }
 
-    static double LookupHeepSFSyst(const double scEta, const double et, const int analysisYear) {
-      if(analysisYear==2016)
-        return ElectronScaleFactors2016::LookupHeepSFSyst(scEta, et);
-      else if(analysisYear==2017)
+    static double LookupHeepSFSyst(const double scEta, const double et, const std::string analysisYear) {
+      if(analysisYear=="2016preVFP")
+        return ElectronScaleFactors2016preVFP::LookupHeepSFSyst(scEta, et);
+      else if(analysisYear=="2016postVFP")
+        return ElectronScaleFactors2016postVFP::LookupHeepSFSyst(scEta, et);
+      else if(analysisYear=="2017")
         return ElectronScaleFactors2017::LookupHeepSFSyst(scEta, et);
-      else if(analysisYear==2018)
+      else if(analysisYear=="2018")
         return ElectronScaleFactors2018::LookupHeepSFSyst(scEta, et);
-      else
-        return -1;
+      throw std::runtime_error("Did not understand year given: "+analysisYear);
     }
 };
